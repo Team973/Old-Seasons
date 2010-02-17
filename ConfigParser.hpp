65cc50847070d95a1eb4cf66ff3e04eeab901409
Ross Light <rlight2@gmail.com>
2010-02-17 07:47:20 -0800

    Made SetDefault return the current value

--- ConfigParser.hpp
+++ ConfigParser.hpp
@@ -30,9 +30,9 @@ public:
 	int GetInt(std::string key) const;
 	double GetDouble(std::string key) const;
 	
-	void SetDefault(std::string key, std::string value);
-	void SetDefault(std::string key, int value);
-	void SetDefault(std::string key, double value);
+	std::string SetDefault(std::string key, std::string value);
+	int SetDefault(std::string key, int value);
+	double SetDefault(std::string key, double value);
 	void Set(std::string key, std::string value);
 	void Set(std::string key, int value);
 	void Set(std::string key, double value);
