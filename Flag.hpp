43187972bf2baf151450d6cd151b2248ccd876c8
Ross Light <rlight2@gmail.com>
2010-02-26 23:12:50 -0800

    Added Flag class

---
+++ Flag.hpp
@@ -0,0 +1,29 @@
+//
+//	Flag.hpp
+//	Team 973
+//	2010 "The Boss"
+//
+//	Created on 2/26/10.
+//
+
+#ifndef _BOSS_973_FLAG_H_
+#define _BOSS_973_FLAG_H_
+
+class Flag
+{
+protected:
+	bool m_value, m_prev;
+public:
+	Flag();
+	Flag(bool);
+	
+	bool Get();
+	void Set(bool);
+	
+	bool Triggered();
+	bool TriggeredOn();
+	bool TriggeredOff();
+	void ClearTrigger();
+};
+
+#endif
