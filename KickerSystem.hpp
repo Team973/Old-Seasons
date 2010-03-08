3fe377a737795196ce5e8bd8ef93d6ec71c4c710
Ross Light <rlight2@gmail.com>
2010-03-08 11:10:37 -0800

    Removed m_drive member

---
+++ KickerSystem.hpp
@@ -0,0 +1,39 @@
+//
+//	KickerSystem.hpp
+//	Team 973
+//	2010 "The Boss"
+//
+//	Created on 2/16/10.
+//
+
+#include "WPILib.h"
+#include "Options.hpp"
+#include "BossRobot.hpp"
+#include "Flag.hpp"
+
+#ifndef _BOSS_973_KICKERSYSTEM_H_
+#define _BOSS_973_KICKERSYSTEM_H_
+
+class KickerSystem
+{
+protected:
+	BossRobot *m_robot;
+	short m_strength;
+	bool m_kicking;
+	Flag m_inDeadband;
+	Flag m_kickTrigger;
+	
+	enum {kStrengthLo, kStrengthMd, kStrengthHi};
+public:
+	KickerSystem(BossRobot *);
+	virtual ~KickerSystem();
+	
+	virtual void ReadControls();
+	virtual void Update();
+	virtual void Kick();
+protected:
+	virtual void UpdateWinch();
+	virtual void UpdateKicker();
+};
+
+#endif
