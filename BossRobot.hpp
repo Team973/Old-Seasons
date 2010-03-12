fd9b3b5ccdc10372c3703356ca8b7b3943bf1cee
Ross Light <rlight2@gmail.com>
2010-03-12 10:55:34 -0800

    Added AbsoluteEncoder class

---
+++ BossRobot.hpp
@@ -0,0 +1,159 @@
+/**
+ *	@file BossRobot.hpp
+ *	Header for the BossRobot class.
+ *
+ *	Team 973<br>
+ *	2010 "The Boss"
+ *
+ *	Created on 2/11/10.
+ */
+
+#include "Options.hpp"
+#include "WPILib.h"
+#include "MyDriverStationLCD.h"
+#include "ConfigParser.hpp"
+
+#ifdef FEATURE_CAMERA
+#	include "Vision/AxisCamera.h"
+#endif
+
+#ifndef _BOSS_973_BOSSROBOT_H_
+#define _BOSS_973_BOSSROBOT_H_
+
+class State;
+class DriveSystem;
+class KickerSystem;
+class ArmSystem;
+
+/**
+ *	Main controller for the robot.
+ *
+ *	Most of what is contained in here is the basic code to get the robot
+ *  initialized and running, and then the core logic is handled by State
+ *	subclasses.
+ */
+class BossRobot : public SimpleRobot
+{
+private:
+	State *m_state, *m_prevState;
+	DriveSystem *m_driveSystem;
+	KickerSystem *m_kickerSystem;
+	ArmSystem *m_armSystem;
+	ConfigParser m_config;
+	
+	SpeedController *m_leftMotor1, *m_leftMotor2;   // Front, Back
+	SpeedController *m_rightMotor1, *m_rightMotor2; // Front, Back
+	Solenoid *m_gearSwitch;
+	Encoder *m_leftDriveEncoder, *m_rightDriveEncoder;
+	AnalogChannel *m_gyroChannel;
+	Gyro *m_gyro;
+	
+	Relay *m_compressor;
+	DigitalInput *m_pressureSwitch;
+	
+	SpeedController *m_shoulderMotor1, *m_shoulderMotor2;
+	AnalogChannel *m_shoulderSensor;
+	
+	SpeedController *m_kickerMotor;
+	Relay *m_kickerWinchRelay1, *m_kickerWinchRelay2;
+	AnalogChannel *m_kickerWinchSensor;
+	AnalogChannel *m_kickerEncoder;
+	SpeedController *m_intakeMotor;
+	
+#ifdef FEATURE_CAMERA
+	AxisCamera *m_camera;
+#endif
+	
+	Timer *m_ioTimer, *m_visionTimer;
+public:
+	/** Initialize the robot */
+	BossRobot();
+	
+	/**
+	 *	Handle autonomous mode.
+	 *
+	 *	This method will be called when autonomous is started, and it is the
+	 *	responsibility of this method to exit when autonomous is over.
+	 */
+	virtual void Autonomous();
+	
+	/**
+	 *	Handle teleoperated mode.
+	 *
+	 *	This method will be called when teleoperated is started, and it is the
+	 *	responsibility of this method to exit when teleoperated is over.
+	 *
+	 *	When this method is called, it will switch the current state to be an
+	 *	instance of NormalState.
+	 */
+	virtual void OperatorControl();
+	
+	/** Get the current state. */
+	inline State *GetState() 				{ return m_state; }
+	
+	/**
+	 *	Switch the current state.
+	 *
+	 *	The new state will not be changed to immediately.  When the run loop
+	 *	hits the next iteration, it will discover that the state has changed,
+	 *	call State::Exit on the old state and State::Enter on the new state.
+	 */
+	void ChangeState(State *);
+	
+	inline DriveSystem *GetDriveSystem() 	{ return m_driveSystem; }
+	void SetDriveSystem(DriveSystem *d);
+	
+	inline KickerSystem *GetKickerSystem() 	{ return m_kickerSystem; }
+	
+	inline ArmSystem *GetArmSystem()		{ return m_armSystem; }
+	
+	/**
+	 *	Get the robot's configuration.
+	 *
+	 *	The values inside the configuration parser are already initialized with
+	 *	the contents of the "boss.cfg" file.
+	 */
+	inline ConfigParser &GetConfig()		{ return m_config; }
+	
+	/**
+	 *	Stop the robot.
+	 *
+	 *	This simply stops all outputs that the robot has control over.  If you
+	 *	update other subsystems, then the robot may not completely stop, but
+	 *	this should suffice for most cases.
+	 *
+	 *	The basic use for this method is making sure that when you go to switch
+	 *	states that there aren't any lingering motors running.
+	 */
+	void SoftStop();
+	
+	// Accessors
+	inline SpeedController *GetLeftFrontDriveMotor()	{ return m_leftMotor1; }
+	inline SpeedController *GetLeftRearDriveMotor()		{ return m_leftMotor2; }
+	inline SpeedController *GetRightFrontDriveMotor()	{ return m_rightMotor1; }
+	inline SpeedController *GetRightRearDriveMotor()	{ return m_rightMotor2; }
+	inline Solenoid *GetGearSwitch()					{ return m_gearSwitch; }
+	inline Encoder *GetLeftDriveEncoder() 				{ return m_leftDriveEncoder; }
+	inline Encoder *GetRightDriveEncoder() 				{ return m_rightDriveEncoder; }
+	inline Gyro *GetGyro() 								{ return m_gyro; }
+	
+	inline Relay *GetKickerWinch1()						{ return m_kickerWinchRelay1; }
+	inline Relay *GetKickerWinch2()						{ return m_kickerWinchRelay2; }
+	inline AnalogChannel *GetKickerWinchSensor()		{ return m_kickerWinchSensor; }
+	inline AnalogChannel *GetKickerEncoder()			{ return m_kickerEncoder; }
+	inline SpeedController *GetKickerMotor()			{ return m_kickerMotor; }
+	inline SpeedController *GetIntakeMotor()			{ return m_intakeMotor; }
+	
+	inline SpeedController *GetShoulderMotor1()			{ return m_shoulderMotor1; }
+	inline SpeedController *GetShoulderMotor2()			{ return m_shoulderMotor2; }
+	inline AnalogChannel *GetShoulderSensor()			{ return m_shoulderSensor; }
+
+protected:
+	void SendIOPortData(void);
+	void SendVisionData(void);
+	
+	void PreStep(void);
+	void PostStep(void);
+};
+
+#endif
---
+++ State.hpp
@@ -0,0 +1,89 @@
+/**
+ *	@file State.hpp
+ *	The interface for the robot state machine
+ *
+ *	Team 973<br>
+ *	2010 "The Boss"
+ *
+ *	Created on 2/11/10.
+ */
+
+#include "WPILib.h"
+#include "BossRobot.hpp"
+
+#ifndef _BOSS_973_STATE_H_
+#define _BOSS_973_STATE_H_
+
+/**
+ *	A certain behavior of the robot.
+ *
+ *	In laymen's terms, each state is a different mode of the robot.  This class
+ *	merely defines the interface that the states must implement so that the
+ *	BossRobot knows how to give control to the state.
+ */
+class State
+{
+protected:
+	BossRobot *m_robot;
+	
+	State()                { m_robot = NULL; }
+	State(BossRobot *r)    { m_robot = r; }
+public:
+	virtual ~State() {};
+	
+	/**
+	 *	Enter the state.
+	 *
+	 *	This will be called once when the State is initially changed into (via
+	 *	BossRobot::ChangeState), but the same instance may be entered several
+	 *	times during the execution of the program.  This is guaranteed to be
+	 *	called before State::Step.
+	 */
+	virtual void Enter() = 0;
+	
+	/**
+	 *	Exit the state.
+	 *
+	 *	This will be called once when the BossRobot class is changing to a new
+	 *	state (via BossRobot::ChangeState), but the same instance may be exited
+	 *	several times during the execution of the program.  This is guaranteed
+	 *	to be called before the new state's State::Enter.
+	 */
+	virtual void Exit() = 0;
+	
+	/**
+	 *	Do the state's logic.
+	 *
+	 *	BossRobot calls this method once for every iteration of the run loop
+	 *	when the state is active.
+	 */
+	virtual void Step() = 0;
+};
+
+/**
+ * The "Get The Frame Upright" state! (TM)
+ */
+class GTFUState : public State
+{
+};
+
+/**
+ *	"User wants to suspend" state.
+ *
+ *	This makes the arm go up and disables kicking.
+ */
+class FinaleState : public State
+{
+public:
+	FinaleState(BossRobot *);
+	
+	virtual void Enter();
+	virtual void Exit();
+	virtual void Step();
+};
+
+class RaisingState : public State
+{
+};
+
+#endif
