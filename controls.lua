885962cdeeb0feacf6df577048319fddd271f3a0
Ross Light <ross@zombiezen.com>
2011-01-17 21:01:08 -0800

    Move arm code to arm.lua

--- controls.lua
+++ controls.lua
@@ -1,8 +1,9 @@
 -- controls.lua
 
 -- Imports
-local wpilib = require("wpilib")
+local arm = require("arm")
 local drive = require("drive")
+local wpilib = require("wpilib")
 
 -- Globals
 local ipairs = ipairs
@@ -20,16 +21,28 @@ controls =
 {
     -- Joystick 1
     {
-        ["y"] = function(axis) drive.arcade(axis, nil) end,
-        [1] = function() drive.setGear(false) end
+        ["y"] = function(axis) drive.arcade(-axis, nil) end,
+        [1] = {down=function() drive.setGear(false) end}
     },
     -- Joystick 2
     {
-        ["x"] = function(axis) drive.arcade(nil, axis) end,
-        [1] = function() drive.setGear(true) end
+        ["x"] = function(axis) drive.arcade(nil, -axis) end,
+        [1] = {down=function() drive.setGear(true) end}
     },
     -- Joystick 3
     {
+        [3] = {
+            down=function() arm.setPreset(2); arm.setManual(false) end,
+            up=function() arm.setManual(true) end
+        },
+        [4] = {
+            down=function() arm.setPreset(1); arm.setManual(false) end,
+            up=function() arm.setManual(true) end
+        },
+        [5] = {
+            down=function() arm.setPreset(3); arm.setManual(false) end,
+            up=function() arm.setManual(true) end
+        },
         [9] = restartRobot
     }
 }
