041327b48758e33492822c3347fde872dfb09f38
Ross Light <ross@zombiezen.com>
2011-01-20 20:58:42 -0800

    Actually update controls.lua's previousState Fix imports in robot.lua Call arm.init on initialization

--- controls.lua
+++ controls.lua
@@ -17,7 +17,7 @@ for i = 1, 3 do
     sticks[i] = wpilib.Joystick(i)
 end
 
-controls =
+defaultControls =
 {
     -- Joystick 1
     {
@@ -43,20 +43,30 @@ controls =
             down=function() arm.setPreset(3); arm.setManual(false) end,
             up=function() arm.setManual(true) end
         },
-        [9] = restartRobot
+        [9] = {down=restartRobot}
     }
 }
 
+-- Initialize previous state table
 local previousState = {}
-
-for i, stick in ipairs(sticks) do
+for i = 1, #sticks do
     previousState[i] = {}
-    for button = 1, 11 do
-        previousState[i][button] = stick:GetRawButton(button)
+end
+
+-- storeState stores the current state of the controls into the previousState
+-- table.
+local function storeState()
+    for i, stick in ipairs(sticks) do
+        for button = 1, 11 do
+            previousState[i][button] = stick:GetRawButton(button)
+        end
     end
 end
 
-local function updateButton(buttonTable, prev, curr)
+storeState()
+
+-- handleButton calls the appropriate event handlers for a single button.
+local function handleButton(buttonTable, prev, curr)
     if curr and not prev then
         if buttonTable.down then buttonTable.down() end
     elseif not curr and prev then
@@ -64,6 +74,7 @@ local function updateButton(buttonTable, prev, curr)
     end
 end
 
+-- update calls the event handlers.
 function update(map)
     for i, stick in ipairs(sticks) do
         local stickMap = map[i]
@@ -72,12 +83,15 @@ function update(map)
         if stickMap.y then stickMap.y(stick:GetY()) end
         -- Update button events
         for button = 1, 11 do
+            local currValue = stick:GetRawButton(button)
             local buttonTable = stickMap[button]
             if buttonTable then
-                updateButton(buttonTable, previousState[i][button], stick:GetRawButton(button))
+                handleButton(buttonTable, previousState[i][button], currValue)
             end
         end
     end
+    -- Save previous state
+    storeState()
 end
 
 -- vim: ft=lua et ts=4 sts=4 sw=4
