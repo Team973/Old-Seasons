99c49d8c72a1226410d51dd33ddac59ea5f4e475
Ross Light <ross@zombiezen.com>
2011-01-27 20:42:35 -0800

    Add practice bot config

---
+++ config/practice.lua
@@ -0,0 +1,49 @@
+-- config/practice.lua
+
+local fakesolenoid = require "fakesolenoid"
+local wpilib = require "wpilib"
+local pid = require "pid"
+
+module(...)
+
+watchdogEnabled = false
+
+features =
+{
+    compressor = true,
+    grabber = false, 
+    gearSwitch = true,
+    lcd = true,
+    softClutch = false,
+}
+
+-- Drive
+leftMotor1 = wpilib.Jaguar(1)
+leftMotor2 = wpilib.Jaguar(3)
+rightMotor1 = wpilib.Jaguar(2)
+rightMotor2 = wpilib.Jaguar(4)
+
+gearSwitch = fakesolenoid.new(4, 2)
+
+flipDriveY = true
+
+-- Arm
+armMotor = wpilib.Jaguar(5)
+armPot = wpilib.AnalogChannel(1)
+armPID = pid.PID:new(8.4, 0, 0)
+armPID.min, armPID.max = -1, 1
+armPos90 = 1.62
+armPos180 = 3.22
+armPresets = {.13, armPos90, armPos180}
+armDriveBackAmplitude = 0.3
+armDriveBackDeadband = 0.1
+if features.grabber then
+    grabberManualSpeed = 0.3
+    grabberMotor = wpilib.Victor(6)
+end
+
+-- Pneumatics
+compressor = wpilib.Relay(4, 1, wpilib.Relay_kForwardOnly)
+pressureSwitch = wpilib.DigitalInput(1)
+
+-- vim: ft=lua et ts=4 sts=4 sw=4
