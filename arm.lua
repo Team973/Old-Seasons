3de8c4119b28f0a5e8b46d2c8d2038a67dbbae75
Ross Light <ross@zombiezen.com>
2011-01-27 18:16:11 -0800

    Backed out to r57

--- arm.lua
+++ arm.lua
@@ -1,24 +1,15 @@
 -- arm.lua
 
-
---Requires--
 local config = require("config")
 local wpilib = require("wpilib")
-local math = require "math"
 
 module(...)
--------------
 
-
---Variables--
 local motor = config.armMotor
 local PID = config.armPID
 local movement = 0
 local manual = true
---------------
 
-
---Functions
 function init()
     PID:reset()
     PID:start()
@@ -37,20 +28,12 @@ function setPreset(preset)
     PID.target = config.armPresets[preset]
 end
 
-
 function update()
-    local armVoltage = config.armPot:GetVoltage()
-    local armAngle = armVoltage * (180 - 90) / (config.armVoltsIn180Degrees - config.armVoltsIn90Degrees)
-    local feedForward = config.armCompensateAmplitude * math.sin(armAngle)
-   if manual then
-        motor:Set(movement + feedForward) --Manual + Feed Forward
-   else
-        motor:Set(-PID:update(config.armPot:GetVoltage()) + feedForward) --PID + Feed Forward
-   end
+    if manual then
+        motor:Set(movement)
+    else
+        motor:Set(-PID:update(config.armPot:GetVoltage()))
+    end
 end
 
-
-
-
---Vim Stuff
 -- vim: ft=lua et ts=4 sts=4 sw=4
--- config/blackknight.lua
+++ config/blackknight.lua
@@ -1,21 +1,17 @@
 -- config/blackknight.lua
 
-
--- Requires --
 local fakesolenoid = require "fakesolenoid"
 local wpilib = require "wpilib"
 local pid = require "pid"
 
 module(...)
---------------
 
 watchdogEnabled = false
-
 
 features =
 {
     compressor = true,
-    grabber = true, 
+    grabber = false, 
     gearSwitch = true,
     lcd = true,
     softClutch = false,
@@ -31,29 +27,16 @@ gearSwitch = fakesolenoid.new(4, 2)
 
 flipDriveY = true
 
-
-
--- Arm --
+-- Arm
 -- TODO: Fill in for actual motors
-
---Arm feedForward 
-armCompensateAmplitude = 1
-armVoltsIn90Degrees = 1.093  
-armVoltsIn180Degrees = 2.67
-
---Motors and PID
 armMotor = wpilib.Jaguar(5)
 armPot = wpilib.AnalogChannel(1)
 armPID = pid.PID:new(8.4, 0, 0)
 armPID.min, armPID.max = -1, 1
-armPresets = {0, 1.16, 2.70}
-
-
--- Grabber --
+armPresets = {.13, 1.16, 2.70}
 if features.grabber then
-    --grabberManualSpeed = 1
-    grabberPickUpMotor = wpilib.Victor(7)
-    grabberWristMotor = wpilib.Jaguar(10)
+    grabberManualSpeed = 0.3
+    grabberMotor = wpilib.Victor(6)
 end
 
 -- Pneumatics
