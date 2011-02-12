b4b49442ccdccd9c9d60085a6797d3a34404fb1c
Ross Light <ross@zombiezen.com>
2011-02-12 13:53:27 -0800

    Rename config files to real robot names

--- config/titan.lua
+++ config/titan.lua
@@ -1,6 +1,6 @@
--- config/practice.lua
+-- config/titan.lua
+-- Competition Bot
 
-local fakesolenoid = require "fakesolenoid"
 local wpilib = require "wpilib"
 local pid = require "pid"
 
@@ -11,7 +11,7 @@ watchdogEnabled = false
 features =
 {
     compressor = true,
-    gearSwitch = false, --Pneumatics not setup on practice bot 
+    gearSwitch = true,
     lcd = true,
     softClutch = false,
 }
@@ -20,23 +20,23 @@ features =
 leftMotor1 = wpilib.Victor(1)
 rightMotor1 = wpilib.Victor(2)
 
-gearSwitch = fakesolenoid.new(4, 2)
+gearSwitch = wpilib.Solenoid(1)
 
-flipDriveY = true
+flipDriveY = false
 
 -- Arm
-armMotor = wpilib.Jaguar(7)
+armMotor = wpilib.Jaguar(5)
 armPot = wpilib.AnalogChannel(1)
-armPID = pid.PID:new(8.4, 0, 0)
+armPID = pid.PID:new(5.0, 0, 0)
 armPID.min, armPID.max = -1, 1
-armPos90 = 1.62
-armPos180 = 3.22
+armPos90 = 1.16
+armPos180 = 2.70
 armPresets = {.13, armPos90, armPos180}
-armDriveBackAmplitude = 0.3
+armDriveBackAmplitude = 0
 armDriveBackDeadband = 0.1
 
-gripMotor = wpilib.Victor(6)
-wristMotor = wpilib.Victor(5)
+gripMotor = wpilib.Victor(7)
+wristMotor = wpilib.Jaguar(10)
 
 -- Pneumatics
 compressor = wpilib.Relay(4, 1, wpilib.Relay_kForwardOnly)
