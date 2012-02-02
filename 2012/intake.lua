e2fb07e6db893742ca90c9c0b2b924b3565b7dd1
Oliver Curry <owcurry@charter.net>
2012-02-01 20:18:27 -0800

    added intake skeleton

--- 2012/intake.lua
+++ 2012/intake.lua
@@ -2,8 +2,29 @@
 
 local wpilib = require("wpilib")
 
+
+
 module(...)
 
--- TODO
+function isballBlockingturret()
+    return false
+end
+
+function isLowered()
+    return false
+end
+
+function toggleRaise()
+end
+
+function update(turretReady)
+end
+
+function setIntakeSpeed(speed) 
+end
+
+function setSideIntakeSpeed(speed)
+end
+
 
 -- vim: ft=lua et ts=4 sts=4 sw=4
