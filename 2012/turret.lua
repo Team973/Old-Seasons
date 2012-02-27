dd86bcc237d5e0f748293a2ecf8af2a29d118d25
Ross Light <ross@zombiezen.com>
2012-02-26 16:49:05 -0800

    Add flywheel speed control

--- 2012/turret.lua
+++ 2012/turret.lua
@@ -1,11 +1,63 @@
 -- turret.lua
-local pid = require("pid")
 
+local pid = require("pid")
 local wpilib = require("wpilib")
 
 module(...)
 
+flywheelPID = pid.new(0.05, 0.0, 0.001)
+local flywheelSpeed = 0.0
+local flywheelPrevPos = 0.0
+local flywheelSpeedTimer = wpilib.Timer()
+
+local flywheelTargetSpeed = 0.0
+local flywheelFeedforward = 19300.0 / 80.0 * 36.0
+local flywheelCounter = wpilib.Counter(2, 6)
+local flywheelMotor = wpilib.Victor(2, 6)
+local flywheelTicksPerRevolution = 2.0
+
+flywheelCounter:Start()
+
 local HARD_LIMIT = 90
+
+-- Calculate the current flywheel speed (in RPM).
+function getFlywheelSpeed()
+    return flywheelSpeed
+end
+
+-- Retrieve the target flywheel speed
+function getFlywheelTargetSpeed(speed)
+    return flywheelTargetSpeed
+end
+
+-- Change the target flywheel speed
+function setFlywheelTargetSpeed(speed)
+    flywheelTargetSpeed = speed
+end
+
+function update()
+    local dashboard = wpilib.SmartDashboard_GetInstance()
+
+    flywheelPID.timer:Start()
+
+    local pos = flywheelCounter:Get() / flywheelTicksPerRevolution -- in revolutions
+    local dt = flywheelPID.timer:Get() / 60.0 -- in minutes
+    flywheelPID.timer:Reset()
+
+    flywheelSpeedTimer:Start()
+    if flywheelSpeedTimer:HasPeriodPassed(0.5) then
+        flywheelSpeed = (pos - flywheelPrevPos) / (0.5 / 60.0)
+        flywheelPrevPos = pos
+    end
+
+    flywheelPID.target = flywheelPID.target + flywheelTargetSpeed * dt
+    local flywheelOutput = flywheelPID:update(pos, dt) + flywheelTargetSpeed * (1/flywheelFeedforward - flywheelPID.d)
+    if flywheelOutput > 0.0 then
+        flywheelMotor:Set(flywheelOutput)
+    else
+        flywheelMotor:Set(0.0)
+    end
+end
 
 function calculateTarget(turretAngle, desiredAngle)
     --calculates shortest desired angle
@@ -28,9 +80,5 @@ function calculateTarget(turretAngle, desiredAngle)
 
     return desiredAngle
 end
-
-	
-
-
 
 -- vim: ft=lua et ts=4 sts=4 sw=4
