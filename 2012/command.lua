5ce9a428f78b6c6910828dea6f17804ce650c7c8
Ross Light <ross@zombiezen.com>
2012-03-22 11:34:13 -0700

    Update README descriptions

---
+++ 2012/command.lua
@@ -0,0 +1,190 @@
+-- command.lua - An implementation of WPILib commands in pure Lua
+
+local table = table
+local ipairs = ipairs
+local setmetatable = setmetatable
+
+module(...)
+
+-- SCHEDULER
+
+local commands, additions, subsystems = {}, {}, {}
+
+local processCommandAddition
+
+-- Run a single iteration of the scheduler.
+function Run()
+    -- Loop through commands
+    for _, command in ipairs(commands) do
+        if not command:_run() then
+            RemoveCommand(command)
+        end
+    end
+
+    -- Add new commands
+    for _, addition in ipairs(additions) do
+        processCommandAddition(addition)
+    end
+    additions = {}
+
+    -- TODO: Add defaults
+end
+
+function AddCommand(command)
+    additions[#additions + 1] = command
+end
+
+function RegisterSubsystem(subsystem)
+    subsystems[#subsystems + 1] = subsystem
+end
+
+function RemoveCommand(command)
+    -- TODO
+end
+
+function processCommandAddition(command)
+end
+
+-- COMMAND
+
+Command = {}
+
+--[[
+attributes:
+    name
+    startTime
+    timeout
+    initialized
+    requirements
+    running
+    interruptible
+    canceled
+    locked
+    runWhenDisabled
+    parent
+    networkTable
+--]]
+
+function Command:New(tbl)
+    local newCommand = {
+        locked=false,
+        initialized=false,
+        running=false,
+        interruptable=false,
+        canceled=false,
+        runWhenDisabled=false,
+    }
+    setmetatable(newCommand, {__index=self})
+    for k, v in pairs(tbl) do
+        newCommand[k] = v
+    end
+    return newCommand
+end
+
+function Command:Initialize()
+end
+
+function Command:Execute()
+end
+
+function Command:IsFinished()
+end
+
+function Command:Interrupted()
+    self:End()
+end
+
+function Command:End()
+end
+
+function Command:Cancel()
+    self.canceled = true
+end
+
+-- internal method for scheduler
+function Command:_run()
+    -- TODO: Check for disabled
+
+    if self.canceled then
+        return false
+    end
+
+    if not self.initialized then
+        self.initialized = true
+        -- TODO: start timing
+        self:Initialize()
+    end
+
+    self:Execute()
+    return not self:IsFinished()
+end
+
+-- SUBSYSTEM
+
+Subsystem = {}
+
+--[[
+attributes:
+name
+currentCommand
+defaultCommand
+
+private:
+_initializedDefaultCommand
+--]]
+
+function Subsystem:New(tbl)
+    local newSubsystem = {
+        locked=false,
+        initialized=false,
+        running=false,
+        interruptable=false,
+        canceled=false,
+        runWhenDisabled=false,
+    }
+    setmetatable(newSubsystem, {__index=self})
+    for k, v in pairs(tbl) do
+        newSubsystem[k] = v
+    end
+    return newSubsystem
+end
+
+-- COMMAND GROUP
+CommandGroup = {}
+setmetatable(CommandGroup, {__index=Command})
+
+function CommandGroup:New(tbl)
+    local newCommandGroup = Command.New(self, {commands={}, currentIndex=nil, children={}})
+    for k, v in pairs(tbl) do
+        newCommandGroup[k] = v
+    end
+    return newCommandGroup
+end
+
+function CommandGroup:IsFinished()
+    return self.currentIndex > #self.commands and #self.children == 0
+end
+
+function CommandGroup:IsInterruptable()
+    if not self.interruptable then
+        return false
+    end
+
+    if self.currentIndex and self.currentIndex <= #self.commands then
+        -- TODO: .m_command
+        local command = self.commands[self.currentIndex]
+        if not command:IsInterruptible() then
+            return false
+        end
+    end
+
+    for _, command in pairs(self.children) do
+        if not command:IsInterruptible() then
+            return false
+        end
+    end
+
+    return true
+end
+
+-- vim: ft=lua et ts=4 sts=4 sw=4
---
+++ 2012/turret.lua
@@ -0,0 +1,269 @@
+-- turret.lua
+
+local ipairs = ipairs
+
+local bit = require("bit")
+local pid = require("pid")
+local math = require("math")
+local string = require("string")
+local wpilib = require("wpilib")
+
+module(...)
+
+local flywheelSpeedTable = {
+    numSamples=10,
+}
+flywheelPID = pid.new(0.0025, 0.0, -0.001,
+    nil,
+    function()
+        return flywheelSpeedTable:average()
+    end)
+
+local dashboard = wpilib.SmartDashboard_GetInstance()
+
+local flywheelTargetSpeed = 0.0
+local flywheelFeedforward = 7800
+local in4 = wpilib.DigitalInput(2, 4)
+local in5 = wpilib.DigitalInput(2, 5)
+local in6 = wpilib.DigitalInput(2, 6)
+local flywheelCounter = wpilib.Counter(in5)
+local flywheelMotor = wpilib.Victor(2, 6)
+local flywheelTicksPerRevolution = 6.0
+
+flywheelCounter:Start()
+
+local hoodEncoder1, hoodEncoder2
+local hoodMotor1= wpilib.Victor(2,7) 
+local hoodMotor2= wpilib.Victor(2,8) 
+hoodPID1 = pid.new(0.01, 0, 0)
+hoodPID1:start()
+hoodPID2 = pid.new(0.01, 0, 0)
+hoodPID2:start()
+runHood = 0
+
+function getHoodTarget()
+    return hoodPID1.target
+end
+
+function setHoodTarget(target)
+    hoodPID1.target = target
+    hoodPID2.target = target
+end
+
+encoder = wpilib.Encoder(2, 2, 2, 3, true, wpilib.CounterBase_k1X)
+encoder:Start()
+motor = wpilib.Jaguar(2, 3) 
+turnPID = pid.new(0.05, 0, 0) 
+
+allowRotate = false
+
+local HARD_LIMIT = 90
+
+local function readVexEncoder(encoder)
+    local val = 0
+    local data = wpilib.new_UINT8array(4)
+
+    -- TODO: The error result should probably be examined.
+    encoder:Read(0x40, 4, data)
+    val = bit.bor(val, bit.lshift(wpilib.UINT8array_getitem(data, 0), 8))
+    val = bit.bor(val, bit.lshift(wpilib.UINT8array_getitem(data, 1), 0))
+    val = bit.bor(val, bit.lshift(wpilib.UINT8array_getitem(data, 2), 24))
+    val = bit.bor(val, bit.lshift(wpilib.UINT8array_getitem(data, 3), 16))
+
+    -- XXX: For a few bits more:
+    --[[
+    if not encoder:Read(0x46, 2, data) then
+        --return nil
+    end
+    val = bit.bor(val, bit.lshift(wpilib.UINT8array_getitem(data, 0), 40))
+    val = bit.bor(val, bit.lshift(wpilib.UINT8array_getitem(data, 1), 32))
+    --]]
+
+    wpilib.delete_UINT8array(data)
+
+    return val
+end
+
+local vexMagic = wpilib.new_UINT8array(3)
+wpilib.UINT8array_setitem(vexMagic, 0, string.byte("V"))
+wpilib.UINT8array_setitem(vexMagic, 1, string.byte("E"))
+wpilib.UINT8array_setitem(vexMagic, 2, string.byte("X"))
+
+local I2C_DELAY = 0.1
+local I2C_SIDECAR = 2
+
+local function initVexEncoder(address, num, terminated)
+    local mod = wpilib.DigitalModule_GetInstance(I2C_SIDECAR)
+    local encoder
+
+    local i2c = mod:GetI2C(0x60)
+    i2c:SetCompatibilityMode(true)
+
+    -- Change address
+    i2c:Write(0x4d, address)
+    wpilib.Wait(I2C_DELAY)
+
+    -- Get encoder at new address
+    encoder = mod:GetI2C(address)
+    encoder:SetCompatibilityMode(true)
+
+    -- Verify
+    dashboard:PutBoolean("VEX Encoder Check " .. num, encoder:VerifySensor(0x08, 3, vexMagic))
+    wpilib.Wait(I2C_DELAY)
+
+    -- Terminate (or not)
+    if terminated then
+        encoder:Write(0x4c, 0xff)
+    else
+        encoder:Write(0x4b, 0xff)
+    end
+    wpilib.Wait(I2C_DELAY)
+
+    return encoder
+end
+
+function initI2C()
+    dashboard:PutString("mode", "I2C Init")
+
+    -- Reset encoder addresses
+    local i2c = wpilib.DigitalModule_GetInstance(I2C_SIDECAR):GetI2C(0x00)
+    i2c:SetCompatibilityMode(true)
+    -- ORDER IS SIGNIFICANT HERE
+    i2c:Write(0x4f, 0x03)
+    wpilib.Wait(I2C_DELAY)
+    i2c:Write(0x4e, 0xca)
+    wpilib.Wait(I2C_DELAY)
+
+    hoodEncoder1 = initVexEncoder(0x20, "1", false)
+    hoodEncoder2 = initVexEncoder(0x22, "2", true)
+end
+
+function getTargetAngle()
+    return turnPID.target
+end
+
+function setTargetAngle(angle)
+    turnPID.target = calculateTarget(encoder:Get()/25, angle)
+end
+
+function setFromJoy(x,y)
+    local THRESHOLD = 0.5
+    if allowRotate then
+        local angle
+        if x*x + y*y > THRESHOLD*THRESHOLD then
+            angle = math.atan2(x, y)
+        else
+            angle = 0
+        end
+        angle = angle*180/math.pi
+        turnPID.target = calculateTarget(encoder:Get()/25, angle)
+    end
+end
+
+-- Calculate the current flywheel speed (in RPM).
+function getFlywheelSpeed()
+    return flywheelSpeedTable:average()
+end
+
+function flywheelSpeedTable:add(val)
+    if #self < self.numSamples then
+        self[#self + 1] = val
+    else
+        for i = 1, #self - 1 do
+            self[i] = self[i + 1]
+        end
+        self[#self] = val
+    end
+end
+
+function flywheelSpeedTable:average()
+    local sum = 0.0
+    for _, val in ipairs(self) do
+        sum = sum + val
+    end
+    return sum / #self
+end
+
+-- Retrieve the target flywheel speed
+function getFlywheelTargetSpeed(speed)
+    return flywheelTargetSpeed
+end
+
+-- Change the target flywheel speed
+function setFlywheelTargetSpeed(speed)
+    flywheelTargetSpeed = speed
+end
+
+function resetFlywheel()
+    flywheelCounter:Reset()
+    flywheelPID.target = 0
+end
+
+function update()
+    -- Turret rotation
+    dashboard:PutBoolean("Input 4", in4:Get())
+    dashboard:PutBoolean("Input 5", in5:Get())
+    dashboard:PutBoolean("Input 6", in6:Get())
+    dashboard:PutInt("TURN.TARGET", turnPID.target)
+    dashboard:PutInt("TURN.ANGLE", encoder:Get()/25)
+    turnPID:update(encoder:Get()/25)
+    motor:Set(turnPID.output)
+
+    -- Add flywheel velocity sample
+    flywheelSpeedTable:add(60.0 / (flywheelCounter:GetPeriod() * flywheelTicksPerRevolution))
+
+    -- Get flywheel position and time
+    flywheelPID.timer:Start()
+    local pos = flywheelCounter:Get() / flywheelTicksPerRevolution -- in revolutions
+    local dt = flywheelPID.timer:Get() / 60.0 -- in minutes
+    flywheelPID.timer:Reset()
+
+    -- Update flywheel PID
+    flywheelPID.target = flywheelPID.target + flywheelTargetSpeed * dt
+    local extraTerm = flywheelTargetSpeed * (1/flywheelFeedforward - flywheelPID.d)
+    flywheelPID.target = math.min(pos - (1 - flywheelPID.d - extraTerm) / flywheelPID.p, flywheelPID.target)
+    local flywheelOutput = flywheelPID:update(pos, dt) + extraTerm
+    if flywheelOutput > 0.0 then
+        flywheelMotor:Set(flywheelOutput)
+    else
+        flywheelMotor:Set(0.0)
+    end
+
+    local e1 = -readVexEncoder(hoodEncoder1)
+    local e2 = readVexEncoder(hoodEncoder2)
+    dashboard:PutDouble("Hood 1", e1)
+    dashboard:PutDouble("Hood 2", e2)
+    hoodPID1:update(e1)
+    hoodPID2:update(e2)
+    hoodMotor1:Set(-hoodPID1.output)
+    hoodMotor2:Set(hoodPID2.output)
+end
+
+function calculateTarget(turretAngle, desiredAngle)
+    --calculates shortest desired angle
+    while desiredAngle - turretAngle > 180 do
+        desiredAngle = desiredAngle - 360
+    end
+    while desiredAngle - turretAngle < -180 do
+        desiredAngle = desiredAngle + 360 
+    end
+
+    --make sure the turret doesn't crash
+    if desiredAngle > HARD_LIMIT then
+        desiredAngle = HARD_LIMIT
+    end
+    if desiredAngle < -HARD_LIMIT then
+        desiredAngle = -HARD_LIMIT
+    end
+
+    return desiredAngle
+end
+
+function fullStop()
+    motor:Set(0.0)
+    flywheelMotor:Set(0.0)
+    hoodMotor1:Set(0.0)
+    hoodMotor2:Set(0.0)
+end
+
+-- vim: ft=lua et ts=4 sts=4 sw=4
