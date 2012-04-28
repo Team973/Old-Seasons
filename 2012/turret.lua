a0a7f542dfe865a28522aa2ac3c4c9df107e0f90
Adam Heard <cromat44@gmail.com>
2012-04-28 09:42:29 -0500

    Change RPM values

--- 2012/turret.lua
+++ 2012/turret.lua
@@ -39,7 +39,7 @@ local currPresetName = nil
 PRESETS = {
     cornerFender={flywheelRPM=3150, hoodAngle=0, targetAngle=-84},
     sideFender={flywheelRPM=3500, hoodAngle=0, targetAngle=-22},
-    key={flywheelRPM=6900, hoodAngle=900, superSoftHoodAngle=750, targetAngle=0, hardFlywheelRPM=6400, superHardFlywheelRPM=6200},
+    key={flywheelRPM=6900, hoodAngle=900, superSoftHoodAngle=750, targetAngle=0, hardFlywheelRPM=6200, superHardFlywheelRPM=6000},
     autoKey={flywheelRPM=6200, hoodAngle=1100, targetAngle=-TURRET_ANGLE_OFFSET},
     bridge={flywheelRPM=7000},
 }
