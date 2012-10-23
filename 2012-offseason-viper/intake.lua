b569fb1df20926a334fdaa54c329b3572c28bc8d
Oliver Curry <owcurry@charter.net>
2012-10-22 21:02:34 -0700

    LIGHTS

---
+++ 2012-offseason-viper/intake.lua
@@ -0,0 +1,76 @@
+-- intake.lua
+
+local ipairs = ipairs
+local type = type
+-- most commented out stuff is for hodge-podge look carefully!
+local io = require("io")
+local math = require("math")
+local linearize = require("linearize")
+local pid = require("pid")
+local string = require("string")
+local wpilib = require("wpilib")
+
+module(...)
+
+local lowered = false
+local frontSpeed = 0 -- front intake roller
+local verticalSpeed = 0
+local repack = false
+
+local verticalConveyer = linearize.wrap(wpilib.Victor(6))
+local cheaterRoller = wpilib.Victor(5)
+local frontIntake = wpilib.Victor(3)
+local intakeSolenoid = wpilib.Solenoid(2)
+
+function setRepack(val)
+    repack = val
+end
+
+function setVerticalSpeed(speed)
+    verticalSpeed = speed
+end
+
+function toggleRaise()
+    lowered = not lowered
+end
+
+function setLowered(val)
+    lowered = val
+end
+
+function update(turretReady)
+    local dashboard = wpilib.SmartDashboard_GetInstance()
+
+    frontIntake:Set(frontSpeed)
+
+    intakeSolenoid:Set(lowered)
+    if repack then
+        verticalSpeed = -1
+        cheaterRoller:Set(1)
+--[[    elseif math.abs(frontSpeed) > math.abs(verticalSpeed) then
+        cheaterRoller:Set(frontSpeed)
+    else
+        cheaterRoller:Set(verticalSpeed)
+--]]
+    end
+
+    verticalConveyer:Set(verticalSpeed)
+end
+
+function setIntake(speed)
+    frontSpeed = speed
+end
+
+function intakeStop()
+    frontSpeed = 0
+    verticalSpeed = 0
+    cheaterSpeed = 0
+end
+
+function fullStop()
+    verticalConveyer:Set(0.0)
+    cheaterRoller:Set(0.0)
+    frontIntake:Set(0.0)
+end
+
+-- vim: ft=lua et ts=4 sts=4 sw=4
---
+++ 2012-offseason/turret.lua
@@ -0,0 +1,215 @@
+-- turret.lua
+
+local ipairs = ipairs
+
+local bit = require("bit")
+local intake = require("intake")
+local pid = require("pid")
+local linearize = require("linearize")
+local math = require("math")
+local string = require("string")
+local wpilib = require("wpilib")
+
+module(...)
+
+local flywheelSpeedTable = {
+    numSamples=25,
+}
+local flywheelSpeedFilter = {
+    prev=0,
+    curr=0,
+    weight=.2,
+}
+local flywheelFired = false
+flywheelPID = pid.new(0.05, 0.0, -0.0007,
+    nil,
+    function()
+        return flywheelSpeedFilter:average()
+    end)
+local flywheelPIDGains = {
+    {0, p=0.025, d=-0.005},
+    {2800, p=0.025,d=-0.005},
+    {3273, p=0.025, d=-0.005},
+    {3275, p=0.025, d=-0.005},
+    {3277, p=0.025, d = -.005}
+}
+local TURRET_ANGLE_OFFSET = 0
+
+local currPresetName = nil
+--Fender preset is 2100
+PRESETS = {
+    Feeder={flywheelRPM=3000, hoodAngle=1100, targetAngle=0},
+    rightKey={flywheelRPM=2150, hoodAngle=450, targetAngle=-15},
+    key={flywheelRPM=2650, hoodAngle=450, superSoftHoodAngle=450, targetAngle=0, superHardHoodAngle = 450, hardHoodAngle =450},
+    autoKey={flywheelRPM=6200, hoodAngle=1100, targetAngle=-TURRET_ANGLE_OFFSET},
+    bridge={flywheelRPM=7000},
+}
+
+local dashboard = wpilib.SmartDashboard_GetInstance()
+
+local flywheelTargetSpeed = PRESETS.key.flywheelRPM
+local flywheelOn = false
+local flywheelFeedforward = math.huge
+local flywheelCounter = wpilib.Counter(wpilib.DigitalInput(3))
+local flywheelMotor = linearize.wrap(wpilib.Victor(4))
+local flywheelTicksPerRevolution = 1.0
+local turretEnabled = true
+local flywheelLights = wpilib.Relay(1, 7, wpilib.Relay_kForward)
+
+flywheelCounter:Start()
+
+
+local HARD_LIMIT = 90
+
+-- Calculate the current flywheel speed (in RPM).
+function getFlywheelSpeed()
+    return flywheelSpeedTable:average()
+end
+
+function flywheelSpeedTable:add(val)
+    if #self < self.numSamples then
+        self[#self + 1] = val
+    else
+        for i = 1, #self - 1 do
+            self[i] = self[i + 1]
+        end
+        self[#self] = val
+    end
+end
+
+function flywheelSpeedTable:average()
+    local sum = 0.0
+    for _, val in ipairs(self) do
+        sum = sum + val
+    end
+    return sum / #self
+end
+
+function getFlywheelFilterSpeed()
+    return flywheelSpeedFilter:average()
+end
+
+function flywheelSpeedFilter:add(val)
+    self.prev, self.curr = self:average(), val
+end
+
+function flywheelSpeedFilter:average()
+    return (1 - self.weight) * self.prev + self.weight * self.curr
+end
+
+function clearFlywheelFired()
+    flywheelFired = false
+end
+
+function getFlywheelFired()
+    return flywheelFired
+end
+
+local function tableStep(t, x)
+    if x < t[1][1] then
+        return t[1]
+    end
+    for i = 2, #t do
+        if x >= t[i - 1][1] and x < t[i][1] then
+            return t[i - 1]
+        end
+    end
+    return t[#t]
+end
+
+-- Retrieve the target flywheel speed
+function getFlywheelTargetSpeed(speed)
+    return flywheelTargetSpeed
+end
+
+-- Change the target flywheel speed
+function setFlywheelTargetSpeed(speed)
+    flywheelTargetSpeed = speed
+    local gain = tableStep(flywheelPIDGains, speed)
+    if gain.p then flywheelPID.p = gain.p else flywheelPID.p = 0 end
+    if gain.i then flywheelPID.i = gain.i else flywheelPID.i = 0 end
+    if gain.d then flywheelPID.d = gain.d else flywheelPID.d = 0 end
+end
+
+function resetFlywheel()
+    flywheelCounter:Reset()
+    flywheelPID.target = 0
+end
+
+function runFlywheel(on, speed)
+    if not flywheelOn and on then
+        resetFlywheel()
+    end
+    flywheelOn = on
+    if speed then
+        setFlywheelTargetSpeed(speed)
+    end
+end
+
+function update()
+    -- Update flywheel target speed from intake's squish meter
+    if currPresetName then
+        setFlywheelTargetSpeed(PRESETS[currPresetName].flywheelRPM)
+    end
+
+    -- Add flywheel velocity sample
+    local speedSample = 60.0 / (flywheelCounter:GetPeriod() * flywheelTicksPerRevolution)
+    flywheelSpeedTable:add(speedSample)
+    flywheelSpeedFilter:add(speedSample)
+
+    if flywheelSpeedTable:average() - flywheelSpeedFilter:average() > 300 then
+        flywheelFired = true
+    end
+
+    -- Get flywheel position and time
+    flywheelPID.timer:Start()
+    local pos = flywheelCounter:Get() / flywheelTicksPerRevolution -- in revolutions
+    local dt = flywheelPID.timer:Get() / 60.0 -- in minutes
+    flywheelPID.timer:Reset()
+
+    -- Update flywheel PID
+    do
+        local speed = flywheelTargetSpeed
+        if not flywheelOn then
+            speed = 0
+        end
+
+        flywheelPID.target = flywheelPID.target + speed * dt
+        local extraTerm = speed * (1/flywheelFeedforward - flywheelPID.d)
+        flywheelPID.target = math.min(pos - (1 - flywheelPID.d - extraTerm) / flywheelPID.p, flywheelPID.target)
+        local flywheelOutput = flywheelPID:update(pos, dt) + extraTerm
+        if flywheelOutput > 0.0 then
+            flywheelMotor:Set(-flywheelOutput)
+        else
+            flywheelMotor:Set(0.0)
+        end
+    end
+
+    if getFlywheelSpeed() >= getFlywheelTargetSpeed() - 100 then
+	flywheelLights:Set(wpilib.Relay_kOn)
+    else
+	flywheelLights:Set(wpilib.Relay_kOff)
+    end
+
+
+    -- Print flywheel diagnostics
+    dashboard:PutDouble("Flywheel Speed", getFlywheelSpeed())
+    dashboard:PutInt("Flywheel Speed(Int)", getFlywheelSpeed())
+    dashboard:PutDouble("Flywheel Target Speed", getFlywheelTargetSpeed())
+end
+
+function fullStop()
+    flywheelMotor:Set(0.0)
+end
+
+function setPreset(name)
+    currPresetName = name
+    local p = PRESETS[name]
+    if not p then return end
+
+    if p.flywheelRPM then
+        setFlywheelTargetSpeed(p.flywheelRPM)
+    end
+end
+
+-- vim: ft=lua et ts=4 sts=4 sw=4
