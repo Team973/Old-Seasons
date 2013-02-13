9977d234a5e15c0acf1e811ec97a374af2e8a687
Oliver Curry <owcurry@charter.net>
2013-02-12 19:43:02 -0800

    beginnings of the arm PID loop

--- 2013/arm.lua
+++ 2013/arm.lua
@@ -1,10 +1,39 @@
 -- arm.lua
 
 local wpilib = require("wpilib")
+local pid = require("pid")
 
 module(...)
 
 encoder = wpilib.Encoder(1, 2)
 motor = wpilib.Talon(8)
 
+armPID = pid.new(0, 0, 0)
+armPid:start()
+
 encoder:Start()
+
+function setArmTarget(val)
+    target = val
+end
+
+function limitArm(x)
+    if x > .3 then
+        x = .3
+    elseif x < -.3 then
+        x = -.3
+    else
+        x = x 
+    end
+    return x
+end
+
+function update()
+
+    armPID:update(encoder:Get()/50 * 3)
+
+    armPID.target = target
+
+
+    motor:Set(limitArm(armPID.output))
+end
