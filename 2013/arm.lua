a52bc9b3d2650297de855372725edf9a3396bda4
Oliver Curry <owcurry@charter.net>
2013-03-02 17:42:28 -0800

    Nuked half the autofire function

--- 2013/arm.lua
+++ 2013/arm.lua
@@ -29,7 +29,7 @@ encoder:Start()
 calibrationPulse:Start()
 
 PRESETS = {
-    Shooting = { armAngle = 34 },
+    Shooting = { armAngle = 35 },
     Loading = { armAngle = 82.25 },
     Stow = { armAngle = 5 },
     Horizontal = { armAngle = 60.0 },
