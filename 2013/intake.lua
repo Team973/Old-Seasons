d61a6473f236ff62d3c0eaebac229a9f35ef20a5
Oliver Curry <owcurry@charter.net>
2013-03-21 21:32:04 -0700

    Nuked the intakePID and added the beginnings of the new controls

--- 2013/intake.lua
+++ 2013/intake.lua
@@ -6,54 +6,21 @@ local wpilib = require("wpilib")
 module(...)
 
 local lowered = false
+local intakeDeploySpeed = .5
 
 local motor = wpilib.Victor(4)
 local intakeRollers = wpilib.Victor(5)
-
-local intakePID = pid.new(0, 0, 0)
-intakePID.min, intakePID.max = -0.3, 0.3
-intakePID:start()
-
-PRESETS = {
-    Down = { intakeAngle = 45 },
-    Stow = { intakeAngle = 5 },
-}
-
-function getIntakeTarget()
-    return intakePID.target
-end
-
-function getIntakeAngle()
-end
-
-function setIntakeTarget(target)
-    intakePID.target = target
-end
 
 function setIntakeSpeed(speed)
     intakeSpeed = speed
 end
 
-function intakeStopCheck(angle)
-    local currAngle = angle - getIntakeAngle()
-    if currAngle >= -.5 and currAngle <= .5 then
-        return true
-    else
-        return false
-    end
-end
-
-function setPreset(name)
-    local p = PRESETS[name]
-    if p then
-        setIntakeTarget(p.intakeAngle)
-    end
+function setIntakeDeploy(val)
 end
 
 function setLowered(val)
     if lowered ~= val and arm.isIntakeDeploySafe() then
         lowered = val
-        setPreset("Down")
     end
 end
 
@@ -63,12 +30,8 @@ end
 
 function update()
     intakeRollers:Set(intakeSpeed)
-
-    if not intakeStopCheck() then
-        motor:Set(intakePID:update(getAngle()))
-    else
-        fullStop()
-    end
+    motor:Set(intakeDeploySpeed)
+    motor:Set(0)
 end
 
 function fullStop()
