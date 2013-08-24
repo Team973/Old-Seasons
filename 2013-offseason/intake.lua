4240b371ffab019d0c557069fe97fa285812368c
Oliver Curry <owcurry@charter.net>
2013-08-24 12:10:12 -0700

    Cleaned up intake.lau

--- 2013-offseason/intake.lua
+++ 2013-offseason/intake.lua
@@ -23,7 +23,7 @@ local INTAKE_LOAD = "intake_load"
 local HUMAN_LOAD = "human_load"
 local DOWN = "down"
 
-local intakePID = pid.new(5, 0, 0)
+local intakePID = pid.new(0, 0, 0)
 intakePID.min, intakePID.max = -1, 1
 intakePID:start()
 
@@ -57,77 +57,8 @@ function setIntakeSpeed(speed)
     intakeSpeed = speed
 end
 
-function goToStow(bool)
-    stowed = bool
-    if stowed then
-        intakeMoveState = STOWED
-        setPreset("Stow")
-        stowed = false
-    end
-end
-
-function goToDeploy(bool)
-    deployed = bool
-    if deployed then
-        intakeMoveState = DEPLOYED
-        setPreset("Deployed")
-        deployed = false
-    end
-end
-
-function goToIntake(bool)
-    intaking = bool
-    if intaking then
-        intakeState = INTAKE_LOAD
-        setPreset("Intake")
-        intaking = false
-    end
-end
-
-function goToHuman(bool)
-    loading = bool
-    if loading then
-        intakeState = HUMAN_LOAD
-        setPreset("Human")
-        loading = false
-    end
-end
-
-function goToDown(bool)
-    down = bool
-    if down then
-        intakeState = DOWN
-        setPreset("Deployed")
-        down = false
-    end
-end
-
 function update()
     intakeRollers:Set(intakeSpeed)
-
-    if intakeMoveState == DEPLOYED  then
-        if intakeState == INTAKE_LOAD then
-            if getAngle() < 4.1 then
-                motor:Set(intakePID:update(getAngle()))
-            else
-                motor:Set(0.0)
-            end
-        elseif intakeState == HUMAN_LOAD  then
-            motor:Set(intakePID:update(getAngle()))
-        elseif intakeState == DOWN then
-            motor:Set(intakePID:update(getAngle()))
-        else
-            motor:Set(intakePID:update(getAngle()))
-        end
-    elseif intakeMoveState == STOWED then
-        if getAngle() > 2.2 then
-            motor:Set(intakePID:update(getAngle()))
-        else
-            motor:Set(0.0)
-        end
-    else
-        motor:Set(0.0)
-    end
 end
 
 function getState()
@@ -142,7 +73,6 @@ function dashboardUpdate()
     wpilib.SmartDashboard_PutNumber("Intake Angle", getAngle())
     wpilib.SmartDashboard_PutNumber("Intake PID Output", intakePID:update(getAngle()))
     wpilib.SmartDashboard_PutNumber("Intake Motor Output", motor:Get())
-    wpilib.SmartDashboard_PutNumber("Intake HIT", 0)
     wpilib.SmartDashboard_PutNumber("Intake Error", getTarget() - getAngle())
     wpilib.SmartDashboard_PutString("Intake State", intakeState)
 end
