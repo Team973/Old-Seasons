9cbf4db28768a2efa8164f09b6e83e359e481cb7
Oliver Curry <owcurry@charter.net>
2013-09-28 19:22:47 -0700

    Got cpp stuff working

---
+++ 2013-cpp/SimpleTemplate/MyRobot.cpp
@@ -0,0 +1,95 @@
+#include "WPILib.h"
+
+/**
+ * This is a demo program showing the use of the RobotBase class.
+ * The SimpleRobot class is the base of a robot application that will automatically call your
+ * Autonomous and OperatorControl methods at the right time as controlled by the switches on
+ * the driver station or the field controls.
+ */ 
+class RobotDemo : public SimpleRobot
+{
+	//RobotDrive myRobot; // robot drive system
+	Joystick stick; // only joystick
+	Compressor comp;
+	Solenoid indexer;
+    Solenoid shotAngle;
+    Talon roller;
+    Talon flywheelMotor;
+    Counter flywheelCounter;
+
+public:
+	RobotDemo(void):
+		//myRobot(1, 2),	// these must be initialized in the same order
+		stick(1),		// as they are declared above.
+		comp(14,8),
+		indexer(1),
+        shotAngle(4),
+        roller(6),
+        flywheelMotor(3),
+        flywheelCounter(1)
+	{
+		//myRobot.SetExpiration(0.1);
+		comp.Start();
+        flywheelCounter.Start();
+	}
+
+	
+	void Autonomous(void)
+	{
+		
+	}
+
+
+	
+	void OperatorControl(void)
+	{
+
+        const int flywheelTicksPerRevolution = 1;
+
+        float computeFlyheelSpeed(float counter)
+        {
+            return 60 / (counter.GetPeriod() * flywheelTicksPerRevolution);
+        }
+
+        float getFlywheelSpeed()
+        {
+            return computeFlywheelSpeed(flywheelCounter.GetPeriod());
+        }
+
+        float RPMcontrol(float rpm)
+        {
+            const int dangerRPM = 10000;
+            const int targetFlywheelRPM = 5500;
+
+            if (rpm > dangerRPM)
+                return 0;
+            else if (rpm < targetFlywheelRPM)
+                return 1;
+            else
+                return .4;
+        }
+
+		//myRobot.SetSafetyEnabled(true);
+		while (IsOperatorControl())
+		{
+			//myRobot.ArcadeDrive(stick); // drive with arcade style (use right stick)
+            indexer.Set(stick.GetRawButton(1));
+            shotAngle.Set(stick.GetRawButton(2));
+            roller.Set(stick.GetRawButton(5));
+            if (stick.GetRawButton(8) == true)
+                flywheelMotor.Set(RPMControl(getFlywheelSpeed());
+
+			Wait(0.005);				// wait for a motor update time
+		}
+	}
+	
+	/**
+	 * Runs during test mode
+	 */
+	void Test() {
+
+	}
+};
+
+START_ROBOT_CLASS(RobotDemo);
+
