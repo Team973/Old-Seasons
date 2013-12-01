6470cce50b5eaf4ae8e48e989ed6c7332be4544f
Oliver Curry <owcurry@charter.net>
2013-12-01 14:29:41 -0800

    Added the auto drive command

---
+++ 2013-cpp/SimpleTemplate/autoDriveCommand.hpp
@@ -0,0 +1,44 @@
+#include "WPILib.h"
+#include "pid.hpp"
+
+class AutoDriveCommand
+{
+protected:
+    float targetX;
+    float targetY;
+    bool backward;
+    float drivePercision;
+    float turnPercision;
+    float driveCap;
+    float turnCap;
+    float arcCap;
+
+    float theta;
+    float magnitude;
+    float deltaX;
+    float deltaY;
+    float prevTheta;
+    float currTheta;
+    float prevGyro;
+    float currGyro;
+    float prevLeft;
+    float currLeft;
+    float prevRight;
+    float currRight;
+    float prevX;
+    float currX;
+    float prevY;
+    float currY;
+
+    double PI;
+
+    PID *drivePID;
+    PID *rotatePID;
+    PID *anglePID;
+public:
+    AutoDriveCommand(float targetX_, float targetY_, bool backward_=false, float drivePercision_=6, float turnPercision_=5, float driveCap_=0.5, float turnCap_=0.7, float arcCap_=0.3);
+    void resetDrive();
+    void calculateDrive();
+    void storeDriveCalculations();
+    bool Run();
+};
