6ceedb910edaabf2fa931df1207f483efabfa2c8
Oliver Curry <owcurry@charter.net>
2013-12-21 15:52:00 -0800

    Checking diffs

---
+++ 2013-cpp/SimpleTemplate/autoCommand.cpp
@@ -0,0 +1,22 @@
+#include "autoCommand.hpp"
+
+AutoCommand::AutoCommand()
+{
+    timer = new Timer();
+}
+
+void AutoCommand::setTimeout(double timeout_)
+{
+    timeout = timeout_;
+}
+
+void AutoCommand::Init()
+{
+    timer->Start();
+    timer->Reset();
+}
+
+bool AutoCommand::Run()
+{
+    return true;
+}
--- 2013-cpp/SimpleTemplate/drive.hpp
+++ 2013-cpp/SimpleTemplate/drive.hpp
@@ -8,26 +8,25 @@ private:
     Talon *frontRightDrive;
     Talon *backLeftDrive;
     Talon *backRightDrive;
+
     Solenoid *lowGear;
     Solenoid *kickUp;
     float oldWheel;
     float negInertiaAccumulator;
     float quickStopAccumulator;
-    float limit(float x);
-    void CheesyDrive(double throttle, double wheel, bool highGear, bool quickTurn);
     bool isLowGear;
     bool isKickUp;
+
 public:
     Drive();
+    float limit(float x);
+    void CheesyDrive(double throttle, double wheel, bool highGear, bool quickTurn);
     void setDriveMotors(float left, float right);
     void setLeftDrive(float speed);
     void setRightDrive(float speed);
-    void update(double DriveX, double DriveY, bool Gear, bool quickTurn);
+    void update(double DriveX, double DriveY, double DriveZ, bool Gear, bool quickTurn);
     void setKickUp(bool k);
     void setBackWheelsDown(bool d);
     void setHighGear(bool g);
-    void setFrontLeftDrive(float speed);
-    void setBackLeftDrive(float speed);
-    void setFrontRightDrive(float speed);
-    void setBackRightDrive(float speed);
+
 };
