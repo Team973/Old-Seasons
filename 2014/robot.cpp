98d8f3e80c89e2fc8e71720a6c4003b79983f36d
Oliver Curry <owcurry@charter.net>
2014-01-04 12:16:52 -0800

    Created 2014 directory

---
+++ 2014/robot.cpp
@@ -0,0 +1,20 @@
+#include "WPILib"
+#include "robot.hpp"
+
+Robot::Robot()
+{
+}
+
+void Robot::Autonomous()
+{
+}
+
+void Robot::OperatorControl()
+{
+}
+
+void Robot::Test()
+{
+}
+
+START_ROBOT_CLASS(Robot);
