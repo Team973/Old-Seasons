5f2cd1e857cd014e870460e2566ce6e088f19848
Oliver Curry <owcurry@charter.net>
2014-01-06 18:16:20 -0800

    Cleaned up base robot files

---
+++ 2014/src/drive.cpp
@@ -0,0 +1,151 @@
+#include "WPILib.h"
+#include "drive.hpp"
+#include <math.h>
+
+Drive::Drive()
+{
+}
+
+float Drive::limit(float x)
+{
+    if (x > 1)
+        return 1;
+    else if (x < -1)
+        return -1;
+
+    return x;
+}
+
+void Drive::setDriveMotors(float left, float right)
+{
+    //TODO(oliver): Add the drivemotors at some point
+}
+
+void Drive::CheesyDrive(double throttle, double wheel, bool highGear, bool quickTurn) {
+  bool isQuickTurn = quickTurn;
+  float turnNonlinHigh = 0.9;
+  float turnNonlinLow = 0.8;
+  float negInertiaHigh = 0.5;
+  float senseHigh = 1.2;
+  float senseLow = 0.6;
+  float senseCutoff = 0.1;
+  float negInertiaLowMore = 2.5;
+  float negInertiaLowLessExt = 5.0;
+  float negInertiaLowLess = 3.0;
+  float quickStopTimeConstant = 0.1;
+  float quickStopLinearPowerThreshold = 0.2;
+  float quickStopStickScalar = 5.0;
+
+  float quickStopAccumulator;
+  double wheelNonLinearity;
+  double oldWheel;
+
+  double negInertia = wheel - oldWheel;
+  oldWheel = wheel;
+
+  double M_PI = 3.141592;
+
+  if (highGear) {
+    wheelNonLinearity = turnNonlinHigh;
+    // Apply a sin function that's scaled to make it feel better.
+    wheel = sin(M_PI / 2.0 * wheelNonLinearity * wheel) / sin(M_PI / 2.0 * wheelNonLinearity);
+    wheel = sin(M_PI / 2.0 * wheelNonLinearity * wheel) / sin(M_PI / 2.0 * wheelNonLinearity);
+  } else {
+    wheelNonLinearity = turnNonlinLow;
+    // Apply a sin function that's scaled to make it feel better.
+    wheel = sin(M_PI / 2.0 * wheelNonLinearity * wheel) / sin(M_PI / 2.0 * wheelNonLinearity);
+    wheel = sin(M_PI / 2.0 * wheelNonLinearity * wheel) / sin(M_PI / 2.0 * wheelNonLinearity);
+    wheel = sin(M_PI / 2.0 * wheelNonLinearity * wheel) / sin(M_PI / 2.0 * wheelNonLinearity);
+  }
+
+  double leftPwm, rightPwm, overPower;
+  float sensitivity = 1.7;
+
+  float angularPower;
+  float linearPower;
+
+  // Negative inertia!
+  static double negInertiaAccumulator = 0.0;
+  double negInertiaScalar;
+  if (highGear) {
+    negInertiaScalar = negInertiaHigh;
+    sensitivity = senseHigh;
+  } else {
+    if (wheel * negInertia > 0) {
+      negInertiaScalar = negInertiaLowMore;
+    } else {
+      if (fabs(wheel) > 0.65) {
+        negInertiaScalar = negInertiaLowLessExt;
+      } else {
+        negInertiaScalar = negInertiaLowLess;
+      }
+    }
+    sensitivity = senseLow;
+
+    if (fabs(throttle) > senseCutoff) {
+      sensitivity = 1 - (1 - sensitivity) / fabs(throttle);
+    }
+  }
+  double negInertiaPower = negInertia * negInertiaScalar;
+  negInertiaAccumulator += negInertiaPower;
+
+  wheel = wheel + negInertiaAccumulator;
+  if(negInertiaAccumulator > 1)
+    negInertiaAccumulator -= 1;
+  else if (negInertiaAccumulator < -1)
+    negInertiaAccumulator += 1;
+  else
+    negInertiaAccumulator = 0;
+
+  linearPower = throttle;
+
+  // Quickturn!
+  if (isQuickTurn) {
+    if (fabs(linearPower) < quickStopLinearPowerThreshold) {
+      double alpha = quickStopTimeConstant;
+      quickStopAccumulator = (1 - alpha) * quickStopAccumulator + alpha * limit(wheel) * quickStopStickScalar;
+    }
+    overPower = 1.0;
+    if (highGear) {
+      sensitivity = 1.0;
+    } else {
+      sensitivity = 1.0;
+    }
+    angularPower = wheel;
+  } else {
+    overPower = 0.0;
+    angularPower = fabs(throttle) * wheel * sensitivity - quickStopAccumulator;
+    if (quickStopAccumulator > 1) {
+      quickStopAccumulator -= 1;
+    } else if (quickStopAccumulator < -1) {
+      quickStopAccumulator += 1;
+    } else {
+      quickStopAccumulator = 0.0;
+    }
+  }
+
+  rightPwm = leftPwm = linearPower;
+  leftPwm += angularPower;
+  rightPwm -= angularPower;
+
+  if (leftPwm > 1.0) {
+    rightPwm -= overPower * (leftPwm - 1.0);
+    leftPwm = 1.0;
+  } else if (rightPwm > 1.0) {
+    leftPwm -= overPower * (rightPwm - 1.0);
+    rightPwm = 1.0;
+  } else if (leftPwm < -1.0) {
+    rightPwm += overPower * (-1.0 - leftPwm);
+    leftPwm = -1.0;
+  } else if (rightPwm < -1.0) {
+    leftPwm += overPower * (-1.0 - rightPwm);
+    rightPwm = -1.0;
+  }
+
+  setDriveMotors(leftPwm, rightPwm);
+}
+
+void Drive::update(double DriveX, double DriveY, bool gear, bool quickTurn)
+{
+    CheesyDrive(DriveY, DriveX, gear, quickTurn);
+}
