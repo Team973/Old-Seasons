c4839dac60f26f576f59e50062eb2398c80ad4f0
Oliver Curry <owcurry@charter.net>
2014-01-21 17:32:30 -0800

    Added a preset system to control arm position

--- 2014/src/arm.hpp
+++ 2014/src/arm.hpp
@@ -4,12 +4,17 @@
 #ifndef ARM_H
 #define ARM_H
 
+#define TEST 1
+#define TEST2 2
+
 class Arm
 {
 public:
     Arm(Talon *motor_, Encoder *sensorA_, Encoder *sensorB, Encoder *sensorC);
     void update();
     void dashboardUpdate();
+    void setTarget(float target);
+    void setPreset(int preset);
 private:
     Talon *motor;
     Encoder *sensorA;
