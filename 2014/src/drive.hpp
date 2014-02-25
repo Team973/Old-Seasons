23d70b8431f9d1cf09d68abc160d4e38199ddbda
Oliver Curry <owcurry@charter.net>
2014-02-24 19:15:05 -0800

    added the is auto value because the wpilib function doesn't work

--- 2014/src/drive.hpp
+++ 2014/src/drive.hpp
@@ -34,7 +34,7 @@ private:
 
 public:
     Drive(Talon *leftDrive_, Talon *rightDrive_, Solenoid *shifters_, Solenoid *kickUp_, Encoder *leftEncoder_, Encoder *rightEncoder_);
-    void update(double DriveX, double DriveY, bool gear, bool kick, bool quickTurn);
+    void update(double DriveX, double DriveY, bool gear, bool kick, bool quickTurn, bool isAuto=false);
     void dashboardUpdate();
 
     float getLeftDrive();
