6b3efb593c095e75ce181715ac94e16626a22a2b
Oliver Curry <owcurry@charter.net>
2014-03-02 10:45:49 -0800

    finished the fire command

--- 2014/src/auto/fireCommand.cpp
+++ 2014/src/auto/fireCommand.cpp
@@ -1,17 +1,14 @@
 #include "WPILib.h"
 #include "fireCommand.hpp"
 
-FireCommand::FireCommand(Shooter *shooter_, float timeout_)
+FireCommand::FireCommand(Shooter *shooter_)
 {
-    setTimeout(timeout_);
-
     shooter = shooter_;
 }
 
 void FireCommand::Init()
 {
-    timer->Start();
-    timer->Reset();
+    shooter->fire(true);
 }
 
 bool FireCommand::Run()
