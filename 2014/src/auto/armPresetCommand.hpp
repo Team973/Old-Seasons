c5123d60675ccba8ff46b33856b09fafc4dfcf88
Oliver Curry <owcurry@charter.net>
2014-03-02 11:18:13 -0800

    created the auto command for moving the arm

---
+++ 2014/src/auto/armPresetCommand.hpp
@@ -0,0 +1,18 @@
+#include "autoCommand.hpp"
+#include "../arm.hpp"
+
+#ifndef ARM_PRESET_COMMAND_H
+#define ARM_PRESET_COMMAND_H
+
+class ArmPresetCommand : public AutoCommand
+{
+public:
+    ArmPresetCommand(Arm *arm_, int preset_);
+    virtual void Init();
+    virtual bool Run();
+private:
+    int preset;
+    Arm *arm;
+};
+
+#endif
