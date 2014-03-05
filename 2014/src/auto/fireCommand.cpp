73a5264a1c7ef2388d8bf2bd53117d150971e966
Oliver Curry <owcurry@charter.net>
2014-03-04 18:02:53 -0800

    refactored the fire command to take time into account

--- 2014/src/auto/fireCommand.cpp
+++ 2014/src/auto/fireCommand.cpp
@@ -1,17 +1,23 @@
 #include "WPILib.h"
 #include "fireCommand.hpp"
 
-FireCommand::FireCommand(Shooter *shooter_)
+FireCommand::FireCommand(Shooter *shooter_, float timeout_)
 {
     shooter = shooter_;
+    setTimeout(timeout_);
 }
 
 void FireCommand::Init()
 {
+    timer->Start();
+    timer->Reset();
     shooter->fire(true);
 }
 
 bool FireCommand::Run()
 {
-    return true;
+    if (timer->Get() > timeout)
+        return true;
+    else
+        return false;
 }
