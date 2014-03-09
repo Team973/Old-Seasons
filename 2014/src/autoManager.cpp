df21cc5848bebcf30a3f655339ef9c9c9ad031d3
Oliver Curry <owcurry@charter.net>
2014-03-09 12:06:48 -0700

    switched back to not using variable distances during auto

--- 2014/src/autoManager.cpp
+++ 2014/src/autoManager.cpp
@@ -68,13 +68,13 @@ void AutoManager::autoSelect(int autoMode)
         case HELLAVATOR_FOREWARD:
             commandSequence.push_back(new ArmPresetCommand(arm, HELLAVATOR, 1));
             commandSequence.push_back(new Hellavator(hellavator, 1));
-            commandSequence.push_back(new LinearDriveCommand(drive, driveDistance, false, 6));
+            commandSequence.push_back(new LinearDriveCommand(drive, 108, false, 6));
             commandSequence.push_back(new AutoWaitCommand(10));
             break;
         case HELLAVATOR_BACKWARD:
             commandSequence.push_back(new ArmPresetCommand(arm, HELLAVATOR, 1));
             commandSequence.push_back(new Hellavator(hellavator, 1));
-            commandSequence.push_back(new LinearDriveCommand(drive, -driveDistance, false, 6));
+            commandSequence.push_back(new LinearDriveCommand(drive, -108, false, 6));
             commandSequence.push_back(new AutoWaitCommand(10));
             break;
         default:
