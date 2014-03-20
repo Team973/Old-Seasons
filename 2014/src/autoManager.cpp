5909a68ad557d891f0aeefe7777041e11f478bcf
Oliver Curry <owcurry@charter.net>
2014-03-20 16:50:35 -0700

    increased the time to drive in auto

--- 2014/src/autoManager.cpp
+++ 2014/src/autoManager.cpp
@@ -50,7 +50,7 @@ void AutoManager::autoSelect(int autoMode)
             break;
         case ONE_BALL_IN_MOVEMENT:
             commandSequence.push_back(new ArmPresetCommand(arm, SHOOTING, 0));
-            commandSequence.push_back(new LinearDriveCommand(drive, 96, false, 5));
+            commandSequence.push_back(new LinearDriveCommand(drive, 120, false, 5));
             commandSequence.push_back(new FireCommand(shooter, 1.5));
             commandSequence.push_back(new AutoWaitCommand(10));
             break;
