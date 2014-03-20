3a169a0f380e339a10a7a205a0b04f8af03f08cf
Oliver Curry <owcurry@charter.net>
2014-03-20 14:55:40 -0700

    populated the corral command

--- 2014/src/auto/corralCommand.cpp
+++ 2014/src/auto/corralCommand.cpp
@@ -2,13 +2,15 @@
 #include "corralCommand.hpp"
 #include "../intake.hpp"
 
-CorralCommand::CorralCommand(Intake *intake_)
+CorralCommand::CorralCommand(Intake *intake_, bool trap_)
 {
     intake = intake_;
+    trap = trap_;
 }
 
 void CorralCommand::Init()
 {
+    intake->dropCorral(trap);
 }
 
 bool CorralCommand::Run()
