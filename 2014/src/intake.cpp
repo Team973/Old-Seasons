a64d4bb716bbab7cf4c0bc7854d1a1f6ebccb644
Oliver Curry <owcurry@charter.net>
2014-03-21 11:32:46 -0700

    stopped the arm from moving in teleop when we get a ball

--- 2014/src/intake.cpp
+++ 2014/src/intake.cpp
@@ -103,7 +103,7 @@ void Intake::update()
                 else if (possesionTimer->Get() >= intakeTime)
                 {
                     possesionTimer->Stop();
-                    arm->setPreset(SHOOTING);
+                    setFangs(true, false);
                     hasBall = true;
                 }
 
