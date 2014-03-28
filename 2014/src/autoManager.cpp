2d9acbf5573328be7a3c6dcf701950fc301a809b
Oliver Curry <owcurry@charter.net>
2014-03-27 17:27:04 -0700

    removed the 1 ball sit still auto

--- 2014/src/autoManager.cpp
+++ 2014/src/autoManager.cpp
@@ -56,12 +56,6 @@ void AutoManager::autoSelect(int autoMode)
             commandSequence.push_back(new AutoWaitCommand(10));
             break;
         case ONE_BALL_SIMPLE:
-            commandSequence.push_back(new ArmPresetCommand(arm, SHOOTING, 2));
-            commandSequence.push_back(new FireCommand(shooter, 2));
-            commandSequence.push_back(new LinearDriveCommand(drive, 60, false, 3));
-            commandSequence.push_back(new AutoWaitCommand(4));
-            break;
-        case ONE_BALL_IN_MOVEMENT:
             commandSequence.push_back(new ArmPresetCommand(arm, SHOOTING, 0));
             commandSequence.push_back(new LinearDriveCommand(drive, 108, false, 5));
             commandSequence.push_back(new AutoWaitCommand(1));
