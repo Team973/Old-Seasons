ce35531f0e23bd87898ebc9a648868507c920f46
Oliver Curry <owcurry@charter.net>
2014-04-05 13:36:28 -0700

    tuned shot angle

--- 2014/src/arm.cpp
+++ 2014/src/arm.cpp
@@ -45,7 +45,7 @@ void Arm::setPreset(int preset)
             errorTarget = 1;
             break;
         case SHOOTING:
-            setTarget(27.5);//27);
+            setTarget(31.5);//27.5);
             autoClamped = false;
             break;
         case STOW:
