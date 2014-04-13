2b6342fe71d1104109ac6f705bf4e1f952249285
Oliver Curry <owcurry@charter.net>
2014-04-12 19:22:01 -0700

    changed the shot angle

--- 2014/src/arm.cpp
+++ 2014/src/arm.cpp
@@ -45,7 +45,7 @@ void Arm::setPreset(int preset)
             errorTarget = 1;
             break;
         case SHOOTING:
-            setTarget(32.5);//27.5);
+            setTarget(33.5);//27.5);
             autoClamped = false;
             break;
         case STOW:
