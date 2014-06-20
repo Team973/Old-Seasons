7e85a15bc7bc4f50fb9a4ace79bccf84ba6edf0e
Oliver Curry <owcurry@charter.net>
2014-06-19 18:37:36 -0700

    tested the log system

--- 2014/src/dataLog.cpp
+++ 2014/src/dataLog.cpp
@@ -7,11 +7,14 @@ DataLog::DataLog(std::string filename_)
     filename = filename_ + ".txt";
 
     // create the file or move and erase the last file
-    if (fopen(filename.c_str(), "r") != NULL)
+    FILE *tmp;
+    tmp = fopen(filename.c_str(), "r");
+    if (tmp != NULL)
     {
-        std::string filePath = "../logs/" + filename;
+        std::string filePath = "logs/" + filename;
         rename(filename.c_str(), filePath.c_str());
     }
+    fclose(tmp);
 
     FILE *file;
     file = fopen(filename.c_str(), "w");
