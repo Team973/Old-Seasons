179dfbc89e0d0d73d763a38d6e33703e4b48f6f2
Oliver Curry <owcurry@charter.net>
2014-07-10 17:03:01 -0700

    tied all the systems together

--- 2014-offseason-test/src/robot.cpp
+++ 2014-offseason-test/src/robot.cpp
@@ -1,8 +1,20 @@
 #include "WPILib.h"
 #include "robot.hpp"
+#include "drive.hpp"
+#include "greyJoy.hpp"
 
 TestRobot::TestRobot()
 {
+    leftDriveMotors = new Talon(1);
+    rightDriveMotors = new Talon(2);
+
+    shiftingSolenoid = new Solenoid(5);
+
+    drive = new Drive(leftDriveMotors, rightDriveMotors, shiftingSolenoid);
+
+    driver = new GreyJoy(1);
+    coDriver = new GreyJoy(2);
+
     this->SetPeriod(0);
 }
 
@@ -25,6 +37,7 @@ void TestRobot::TeleopInit() {
 }
 
 void TestRobot::TeleopPeriodic() {
+    drive->CheesyDrive(driver->getAxis("y"), driver->getAxis("rx"), driver->getButton("rbumper"), driver->getButton("lbumper"));
 }
 
 void TestRobot::TestInit() {
