d000b2f63a944bb537d71b6b3e64531f88d912bc
Oliver Curry <owcurry@charter.net>
2014-08-07 14:36:13 -0700

    created drive command specifically for working with trapezoidal profile generators

---
+++ 2014/src/auto/driveProfileCommand.cpp
@@ -0,0 +1,34 @@
+#include "WPILib.h"
+#include <vector>
+#include "../drive.hpp"
+#include "../trapProfile.hpp"
+#include <math.h>
+#include <vector>
+#include "driveProfileCommand.hpp"
+
+DriveProfileCommand::DriveProfileCommand(Drive *drive_, float target_, float aMax, float vMax, float dMax, float timeout_)
+{
+    drive = drive_;
+    target = target_;
+
+    profileGenerator = new TrapProfile(target, vMax, aMax, dMax);
+
+    setTimeout(timeout_);
+}
+
+void DriveProfileCommand::Init()
+{
+    timer->Start();
+    timer->Reset();
+    drive->setLinear(profileGenerator);
+}
+
+bool DriveProfileCommand::Run()
+{
+    if (fabs(target - drive->getWheelDistance()) >= 2 || timer->Get() >= timeout)
+    {
+        return true;
+    }
+
+    return false;
+}
