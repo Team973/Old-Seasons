ea37afb8949a81deb33c137fd88ee705aef4f481
Oliver Curry <owcurry@charter.net>
2014-08-12 20:03:58 -0700

    created basic turn command that utilizes the trap profile generator

---
+++ 2014/src/auto/turnProfileCommand.cpp
@@ -0,0 +1,31 @@
+#include "WPILib.h"
+#include "turnProfileCommand.hpp"
+#include "../drive.hpp"
+#include "../trapProfile.hpp"
+#include <math.h>
+
+TurnProfileCommand::TurnProfileCommand(Drive *drive_, float target_, float aMax, float vMax, float dMax, float timeout_)
+{
+    drive = drive_;
+    target = target_;
+    profileGenerator = new TrapProfile(target, vMax, aMax, dMax);
+
+    setTimeout(timeout_);
+}
+
+void TurnProfileCommand::Init()
+{
+    timer->Start();
+    timer->Reset();
+    drive->setAngular(profileGenerator);
+}
+
+bool TurnProfileCommand::Run()
+{
+    if ((fabs(target - drive->getGyroAngle()) <= 3 && drive->getVelocity() < 2) || timer->Get() >= timeout)
+    {
+        return true;
+    }
+
+    return false;
+}
