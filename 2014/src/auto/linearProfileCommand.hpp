482196ba8b3e87a1ce7890513d5ca25106460833
Oliver Curry <owcurry@charter.net>
2014-08-12 20:36:52 -0700

    renamed driveProfileCommand to linearProfileCommand

--- 2014/src/auto/linearProfileCommand.hpp
+++ 2014/src/auto/linearProfileCommand.hpp
@@ -3,10 +3,10 @@
 #ifndef DRIVE_PROFILE_COMMAND_H
 #define DRIVE_PROFILE_COMMAND_H
 
-class DriveProfileCommand : public AutoCommand
+class LinearProfileCommand : public AutoCommand
 {
 public:
-    DriveProfileCommand(Drive *drive_, float target_, float aMax, float vMax, float dMax, float timeout_);
+    LinearProfileCommand(Drive *drive_, float target_, float aMax, float vMax, float dMax, float timeout_);
     virtual void Init();
     virtual bool Run();
 private:
