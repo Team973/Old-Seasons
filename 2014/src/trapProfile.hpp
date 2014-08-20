4dcd6c315e9631d4bac3072bc983cc621d71aeb3
Oliver Curry <owcurry@charter.net>
2014-08-19 20:02:01 -0700

    added a return target function for trapProfile

--- 2014/src/trapProfile.hpp
+++ 2014/src/trapProfile.hpp
@@ -7,8 +7,10 @@ class TrapProfile
 public:
     TrapProfile(float xTarget_, float vMax_, float aMax_, float dMax_);
     TrapProfile();
+    float getTarget();
     std::vector<float> getProfile(float loopTime);
 private:
+    float realTarget;
     float xTarget;
     float vMax;
     float aMax;
