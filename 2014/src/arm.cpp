232a3441ec8d1b62f05247c93110716326513448
Oliver Curry <owcurry@charter.net>
2014-08-29 19:29:18 -0700

    changed low goal angle

--- 2014/src/arm.cpp
+++ 2014/src/arm.cpp
@@ -53,7 +53,7 @@ void Arm::setPreset(int preset)
             autoClamped = false;
             break;
         case CLOSE_SHOT:
-            setTarget(7.00);
+            setTarget(8.00);
             autoClamped = false;
             break;
         case HELLAVATOR:
