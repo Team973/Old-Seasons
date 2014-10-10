9cc22b17d003f78b1fbe519a7815861eb7f8a631
Oliver Curry <owcurry@charter.net>
2014-10-09 22:59:33 -0700

    tested new setup

--- 2014-offseason-test/src/robot.cpp
+++ 2014-offseason-test/src/robot.cpp
@@ -1,6 +1,8 @@
 #include "WPILib.h"
 #include "robot.hpp"
 #include "drive.hpp"
+#include "arm.hpp"
+#include "intake.hpp"
 
 Robot::Robot()
 {
@@ -8,10 +10,20 @@ Robot::Robot()
 
     leftDriveMotors = new Talon(1);
     rightDriveMotors = new Talon(2);
+    armMotor = new Talon(3);
+    intakeMotor = new Victor(6);
 
     shiftingSolenoid = new Solenoid(5);
+    clawSolenoid = new Solenoid(3);
+
+    armEncoder = new Encoder(6,7);
+    armEncoder->Start();
 
     drive = new Drive(leftDriveMotors, rightDriveMotors, shiftingSolenoid);
+    arm = new Arm(armMotor, armEncoder);
+    intake = new Intake(intakeMotor, clawSolenoid);
+
+    stick = new Joystick(1);
 }
 
 void Robot::RobotInit() {
@@ -33,7 +45,24 @@ void Robot::TeleopInit() {
 }
 
 void Robot::TeleopPeriodic() {
+
+    if (stick->GetRawButton(2))
+        arm->setBehavior("intake");
+
+    if (stick->GetRawButton(5))
+        arm->setBehavior("stow");
+
+    intake->setSpeed(stick->GetY());
+
+    if (stick->GetRawButton(7))
+        intake->setFangs(true);
+
+    if (stick->GetRawButton(8))
+        intake->setFangs(false);
+
     drive->update();
+    arm->update();
+    intake->update();
 }
 
 void Robot::TestInit() {
