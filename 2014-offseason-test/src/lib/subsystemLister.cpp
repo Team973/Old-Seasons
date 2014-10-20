88b787135006e77b9d526cf8ca3c19c8d0fea3fc
Oliver Curry <owcurry@charter.net>
2014-10-19 18:10:12 -0700

    Created a subsystem lister to remove need to access systems through constructor passing

---
+++ 2014-offseason-test/src/lib/subsystemLister.cpp
@@ -0,0 +1,47 @@
+#include "subsystemLister.hpp"
+#include "subsystemBase.hpp"
+#include "../utility.hpp"
+
+SubsystemLister* SubsystemLister::singleInstance = NULL;
+
+SubsystemLister* SubsystemLister::getInstance()
+{
+    if (singleInstance == NULL)
+    {
+        singleInstance = new SubsystemLister();
+    }
+    return singleInstance;
+}
+
+SubsystemLister::SubsystemLister()
+{
+}
+
+void SubsystemLister::addSubsystem(std::string name, SubsystemBase *s)
+{
+    subsystems[asLower(name)] = s;
+}
+
+SubsystemBase* SubsystemLister::addReturnSubsystem(std::string name, SubsystemBase *s)
+{
+    subsystems[asLower(name)] = s;
+    return s;
+}
+
+bool SubsystemLister::isSubsystem(std::string name)
+{
+    if (subsystems[asLower(name)] != NULL)
+        return true;
+
+    return false;
+}
+
+SubsystemBase* SubsystemLister::getSubsystem(std::string name)
+{
+    if (isSubsystem(name))
+    {
+        return subsystems[asLower(name)];
+    }
+
+    return NULL;
+}
