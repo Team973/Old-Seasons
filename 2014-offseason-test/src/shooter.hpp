014a62f17e6c0ca10b430d8bf673f2e250538ca6
Oliver Curry <owcurry@charter.net>
2014-10-21 22:20:04 -0700

    created shooters

---
+++ 2014-offseason-test/src/shooter.hpp
@@ -0,0 +1,37 @@
+#ifndef SHOOTER_HPP
+#define SHOOTER_HPP
+
+#include <string>
+#include "WPILib.h"
+#include "lib/subsystemBase.hpp"
+
+class PID;
+
+class Shooter : public SubsystemBase
+{
+public:
+    Shooter(Victor *winchMotor_, Solenoid *winchRelease_, DigitalInput *fullCockPoint_);
+    void setBehavior(std::string preset);
+    void cock(std::string preset);
+    void wantFire();
+    bool isFiring();
+    void update();
+private:
+    void setTarget(float target);
+
+    Victor *winchMotor;
+    Solenoid *winchRelease;
+    DigitalInput *fullCockPoint;
+
+    PID *winchPID;
+
+    Timer *cockTimer;
+    Timer *fireTimer;
+
+    std::string currPreset;
+    bool firing;
+
+    map<std::string, float> presets;
+};
+
+#endif
