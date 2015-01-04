579afceac0cda58f02ff8b633e3bdbb6191f68f5
Oliver Curry <owcurry@charter.net>
2015-01-04 10:50:14 -0800

    initial commit for 2015

---
+++ 2015/frc973/src/robot.cpp
@@ -0,0 +1,38 @@
+#include "WPILib.h"
+#include "robot.hpp"
+
+namespace frc973 {
+
+Robot::Robot()
+{
+}
+
+void Robot::RobotInit()
+{
+}
+
+void Robot::AutonomousInit()
+{
+}
+
+void Robot::AutonomousPeriodic()
+{
+
+}
+
+void Robot::TeleopInit()
+{
+
+}
+
+void Robot::TeleopPeriodic()
+{
+}
+
+void Robot::TestPeriodic()
+{
+}
+
+}
+
+START_ROBOT_CLASS(frc973::Robot);
