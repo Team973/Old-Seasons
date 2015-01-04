a5a919207a424022f4527410ef290a38390f17f3
Oliver Curry <owcurry@charter.net>
2015-01-04 11:09:27 -0800

    created drive subsystem

--- 2015/frc973/src/subsystems/drive.hpp
+++ 2015/frc973/src/subsystems/drive.hpp
@@ -4,12 +4,17 @@ namespace frc973 {
 
 class Drive {
 public:
-    Drive();
+    Drive(Talon *leftFrontMotor_, Talon *rightFrontMotor_, Talon *leftBackMotor_, Talon *rightBackMotor_);
     void arcade(float move_, float rotate_);
     void CheesyDrive(double throttle, double wheel, bool highGear, bool quickTurn);
     void update();
 private:
     void setDriveMotors(float left, float right);
+
+    //Cheesy Drive Variables
+    float oldWheel;
+    float negInertiaAccumulator;
+    float quickStopAccumulator;
 };
 
 }
