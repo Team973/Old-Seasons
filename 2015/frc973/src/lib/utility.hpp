4ad6b1ab452fc37bfeeca2935ccc7b8a00356cea
Oliver Curry <owcurry@charter.net>
2015-01-05 13:47:06 -0800

    ported over some string utility functions

--- 2015/frc973/src/lib/utility.hpp
+++ 2015/frc973/src/lib/utility.hpp
@@ -5,6 +5,8 @@
 #include <string>
 #include <cctype>
 #include <sstream>
+#include <string>
+#include <vector>
 
 namespace frc973 {
 
@@ -42,6 +44,54 @@ inline std::string asString(T in)
     std::stringstream ss;
     ss << in;
     return ss.str();
+}
+
+inline std::string scrape(std::string line, char token)
+{
+    unsigned long point = line.find(token);
+    if (point == std::string::npos)
+    {
+        return line;
+    }
+    //printf("preparing to scrape file\n");
+    while ((point = line.find(token)) != std::string::npos)
+    {
+        line.erase(line.begin()+point);
+    }
+    //printf("file scraped\n");
+    return line;
+}
+
+inline std::vector<std::string> split(std::string line, char token, int lineNumber)
+{
+    std::string str;
+    std::vector<std::string> chunks;
+    unsigned long point = line.find(token);
+    int n = 0;
+    
+    if (point == std::string::npos)
+    {
+        printf("The line %i does not contain the character '%c'\n", lineNumber, token);
+        str.append(line);
+        chunks.push_back(str);
+        return chunks;
+    }
+    
+    // this loop removes the first part of the string
+    //printf("preparing to split the file\n");
+    while ((point = line.find(token)) != std::string::npos)
+    {
+        str.append(line, 0, point);
+        line.erase(line.begin(), line.begin()+point+1);
+        chunks.push_back(str);
+        str.erase();
+        n += 1;
+    }
+    //printf("file split\n");
+    
+    str.append(line);
+    chunks.push_back(str);
+    return chunks;
 }
 
 template<class T, class R>
