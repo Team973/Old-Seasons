58da26e97fc213dc0a718b37dc9d1192f0728de7
Oliver Curry <owcurry@charter.net>
2015-01-04 22:21:29 -0800

    added basic strafe

--- 2015/frc973/src/subsystems/drive.hpp
+++ 2015/frc973/src/subsystems/drive.hpp
@@ -4,12 +4,20 @@ namespace frc973 {
 
 class Drive {
 public:
-    Drive(Talon *leftFrontMotor_, Talon *rightFrontMotor_, Talon *leftBackMotor_, Talon *rightBackMotor_);
+    Drive(Talon *leftFrontMotor_, Talon *rightFrontMotor_, Talon *leftBackMotor_, Talon *rightBackMotor_, Talon *strafeMotor_);
     void arcade(float move_, float rotate_);
+    void strafe(float strafe);
     void CheesyDrive(double throttle, double wheel, bool highGear, bool quickTurn);
     void update();
 private:
     void setDriveMotors(float left, float right);
+
+    Talon *leftFrontMotor;
+    Talon *rightFrontMotor;
+    Talon *leftBackMotor;
+    Talon *rightBackMotor;
+
+    Talon *strafeMotor;
 
     //Cheesy Drive Variables
     float oldWheel;
