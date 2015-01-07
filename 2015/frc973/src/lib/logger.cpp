11194432d237c8ed755c78a208576bbe1185d0f7
Oliver Curry <owcurry@charter.net>
2015-01-06 22:59:26 -0800

    got multi-threading working for logging

--- 2015/frc973/src/lib/logger.cpp
+++ 2015/frc973/src/lib/logger.cpp
@@ -1,4 +1,5 @@
 #include "logger.hpp"
+#include <unistd.h>
 
 namespace frc973 {
 
@@ -12,7 +13,9 @@ void Logger::Log(std::string msg) {
 // Run is run in a separate thread, started when the robot is initialized.
 void* Logger::Run(void*) {
     while (true) {
-        std::string msg = msgChan.recv();
+        //std::string msg = msgChan.recv();
+        printf("Hello, World from a thread!!!!\n");
+        usleep(10000);
     }
 }
 
