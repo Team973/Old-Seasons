94227fe54a24c0070133a5d2e518b03bcf431ef2
Oliver Curry <owcurry@charter.net>
2015-01-10 13:20:22 -0800

    added a drive command

---
+++ 2015/frc973/src/auto/driveCommand.hpp
@@ -0,0 +1,16 @@
+#ifndef DRIVE_COMMAND_HPP
+#define DRIVE_COMMAND_HPP
+
+#include "autoCommand.hpp"
+
+namespace frc973 {
+
+class DriveCommand : public AutoCommand {
+public:
+    DriveCommand();
+protected:
+private:
+};
+
+}
+#endif
