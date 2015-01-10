1fd939dd6a6a808afef0ca4469117593cd543ecd
Oliver Curry <owcurry@charter.net>
2015-01-10 10:24:22 -0800

    added some functionality to saving past log files

--- 2015/frc973/src/lib/logger.cpp
+++ 2015/frc973/src/lib/logger.cpp
@@ -1,10 +1,24 @@
 #include "logger.hpp"
 #include "txtFileIO.hpp"
+#include <time.h>
 #include <unistd.h>
 
 namespace frc973 {
 
 Channel<std::string> Logger::msgChan(3);
+
+void Logger::Initialize()
+{
+    time_t rawTime;
+    struct tm * timeInfo;
+    char buffer[80];
+
+    time(&rawTime);
+    timeInfo = localtime(&rawTime);
+    strftime(buffer, 80, "%x::%X", timeInfo);
+
+    rename("/home/lvuser/log.txt", "/home/lvuser/Logs/log::" + buffer + ".log"); 
+}
 
 // Log is called to log a message.
 void Logger::Log(std::string msg) {
