5e5d528f3de86319e1541bb5511c5bcbb1e1d1ac
Oliver Curry <owcurry@charter.net>
2015-01-11 12:39:13 -0800

    this is for the last commit: xymanager.hpp

--- 2015/frc973/src/subsystems/locator.cpp
+++ 2015/frc973/src/subsystems/locator.cpp
@@ -114,6 +114,7 @@ void Locator::update()
     currPoint->x = currX;
     currPoint->y = currY;
     currPoint->angle = currGyro;
+    currPoint->distance = ((currLeft + currRight)/2)/12;
 
     storeCalculations();
 }
