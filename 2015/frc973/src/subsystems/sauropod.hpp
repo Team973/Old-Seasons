ab63ec440af899a03b511c9ecaafd8e2e4ac9819
Oliver Curry <owcurry@charter.net>
2015-01-27 20:29:55 -0800

    created sauropod subsystem

---
+++ 2015/frc973/src/subsystems/sauropod.hpp
@@ -0,0 +1,35 @@
+#ifndef SAUROPOD_HPP
+#define SAUROPOD_HPP
+
+#include <string>
+#include <map>
+
+namespace frc973 {
+
+class PID;
+
+class Sauropod {
+
+    struct Preset {
+        float horizProjection=0;
+        float height=0;
+    };
+
+public:
+    Sauropod();
+    void setPreset(std::string preset);
+    void setTarget(Preset target);
+    void update();
+private:
+
+    PID *armPID;
+    PID *elevatorPID;
+
+    std::string currPreset;
+
+    std::map<std::string, Preset> presets;
+};
+
+}
+
+#endif
