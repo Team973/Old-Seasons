1703b5fb77c2bd803ca11a1aee94a0b634ab95f9
LoganW973 <LoganW973@localhost>
2015-02-03 18:22:58 -0800

    add controlManager

---
+++ 2015/frc973/src/controlManager.cpp
@@ -0,0 +1,17 @@
+/*
+ * controlManager.cpp
+ *
+ *  Created on: Feb 3, 2015
+ *      Author: Logan Wilt
+ */
+
+#include <controlManager.hpp>
+
+namespace frc973 {
+
+ControlManager::ControlManager() {
+	// TODO Auto-generated constructor stub
+
+}
+
+} /* namespace frc973 */
---
+++ 2015/frc973/src/controlManager.hpp
@@ -0,0 +1,20 @@
+/*
+ * controlManager.hpp
+ *
+ *  Created on: Feb 3, 2015
+ *      Author: Logan Wilt
+ */
+
+#ifndef CONTROLMANAGER_HPP_
+#define CONTROLMANAGER_HPP_
+
+namespace frc973 {
+
+class ControlManager {
+public:
+	ControlManager();
+};
+
+} /* namespace frc973 */
+
+#endif /* CONTROLMANAGER_HPP_ */
