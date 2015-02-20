03ed637803d3a0f90e5ed0ca3bfda51f59547094
LoganW973 <LoganW973@localhost>
2015-02-19 17:57:06 -0800

    added containerGrabber subsystem

---
+++ 2015/frc973/src/subsystems/containerGrabber.cpp
@@ -0,0 +1,14 @@
+#include "WPILib.h"
+#include "containerGrabber.hpp"
+
+namespace frc973 {
+
+ContainerGrabber::ContainerGrabber(Solenoid* pinSolenoid_) {
+    
+    pinSolenoid = pinSolenoid_;
+}
+
+void update() { //Put at Bottom
+}
+
+} /* namespace frc973 */
