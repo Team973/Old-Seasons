68740daa2c64ad86af844cdfd1615193a688cfb9
Oliver Curry <owcurry@charter.net>
2015-03-12 21:02:51 -0700

    retracted the foot when the container is clamped

--- 2015/frc973/src/stateManager.cpp
+++ 2015/frc973/src/stateManager.cpp
@@ -241,6 +241,7 @@ void StateManager::update() {
                     }
                     break;
                 case DEAD:
+                    intake->actuateFoot(false);
                     break;
             }
             break;
