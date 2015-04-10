8d05486dedf5eb011f6fd0499122574eaaa6352e
Oliver Curry <owcurry@charter.net>
2015-04-09 19:38:20 -0700

    added grabManager

---
+++ 2015/frc973/src/grabManager.cpp
@@ -0,0 +1,27 @@
+#include "WPILib.h"
+#include "grabManager.hpp"
+#include "subsystems/containerGrabber.hpp"
+#include "subsystems/drive.hpp"
+
+namespace frc973 {
+
+GrabManager::GrabManager(Drive* drive_, ContainerGrabber* grabber_) {
+    drive = drive_;
+    grabber = grabber_;
+
+    waitForContact = false;
+}
+
+void GrabManager::startSequence(float speed, bool wait) {
+    waitForContact = wait;
+    grabber->startGrabSequence(speed);
+}
+
+void GrabManager::cancelSequence() {
+    grabber->cancelGrabSequence();
+}
+
+void GrabManager::update() {
+}
+
+}
