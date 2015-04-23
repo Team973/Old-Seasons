00f90b1f4328f6d0af9158a261bdffb0e0831418
Oliver Curry <owcurry@charter.net>
2015-04-23 18:38:01 -0500

    switched to 3,4,5 capping only

--- 2015/frc973/src/subsystems/sauropod.cpp
+++ 2015/frc973/src/subsystems/sauropod.cpp
@@ -36,8 +36,6 @@ Sauropod::Sauropod(VictorSP* elevatorMotor_, Encoder* elevatorEncoder_, Solenoid
     addPreset("containerLoad", 0);
     addPreset("upperLimit", 48);
     addPreset("rest", 6);
-    addPreset("capOne", 15);
-    addPreset("capTwo", 25);
     addPreset("capThree", 30);
     addPreset("capFour", 40);
     addPreset("capFive", 56);
